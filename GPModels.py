47c1b288617a76e3ae3f905bc261b8f9abcb28c1
Kenza Tazi <kt2015@ic.ac.uk>
2020-05-02 16:54:52 +0100

    simple_gp() and sklearn_gp()

--- GPModels.py
+++ GPModels.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import PrecipitationDataExploration as pde
 
+import gpflow
+import tensorflow as tf
+
+from gpflow.utilities import print_summary
+
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import ExpSineSquared
 from sklearn.model_selection import train_test_split
@@ -13,11 +18,11 @@ tp_ensemble_filepath ='/Users/kenzatazi/Downloads/adaptor.mars.internal-15879875
 mpl_filepath = '/Users/kenzatazi/Downloads/era5_msl_monthly_1979-2019.nc'
 mask_filepath = '/Users/kenzatazi/Downloads/ERA5_Upper_Indus_mask.nc'
 
-da = pde.apply_mask(tp_filepath, mask_filepath)
+da = pde.apply_mask(tp_ensemble_filepath, mask_filepath)
 
 # Simple Gaussian Process Model
 
-def simple_gaussian_model(da):
+def sklearn_gp(da):
     """ Returns trained model """
 
     version_da = da.sel(expver=1) 
@@ -71,5 +76,64 @@ def simple_gaussian_model(da):
     plt.show()
 
 
+def simple_gp(da):
+
+    version_da = da.sel(expver=1) 
+    std_da = version_da.std(dim='number')
+    mean_da = version_da.mean(dim='number')
+
+    gilgit_mean = mean_da.interp(coords={'longitude':74.4584, 'latitude':35.8884 }, method='nearest')
+    gilgit_std = std_da.interp(coords={'longitude':74.4584, 'latitude':35.8884 }, method='nearest')
+
+    multi_index_df_mean = gilgit_mean.to_dataframe('Precipitation')
+    df_mean= multi_index_df_mean.reset_index()
+    df_mean_clean = df_mean.dropna()
+    df_mean_clean['time'] = df_mean_clean['time'].astype('int')
+
+    multi_index_df_std = gilgit_std.to_dataframe('Precipitation')
+    df_std = multi_index_df_std.reset_index()
+    df_std_clean = df_std.dropna()
+    df_std_clean['time'] = df_std_clean['time'].astype('int')
+
+    y = df_mean_clean['Precipitation'].values*1000
+    dy = df_std_clean['Precipitation'].values*1000
+
+    X_prime = df_mean_clean['time'].values.reshape(-1, 1)
+    X = (X_prime - X_prime[0])/ (1e9*60*60*24*365)
+    X_train = X[0:120]
+    # X_test = X[400:-1]
+
+    y_train = y[0:120]
+    dy_train = dy[0:120]
+    # y_test = y[400:-1]
+    # train_test_split(X, y, test_size=0.5, random_state=42)
+
+    # model construction
+    k1= gpflow.kernels.Periodic(gpflow.kernels.RBF(lengthscales=0.3, variance=6))
+    m = gpflow.models.GPR(data=(X_train, y_train.reshape(-1,1)), kernel=k, mean_function=None)  
+    
+    opt = gpflow.optimizers.Scipy()
+    opt_logs = opt.minimize(m.training_loss, m.trainable_variables)
+    print_summary(m)
+
+    X_predictions = np.linspace(0,10,1000)
+    X_plot = X_predictions.reshape(-1, 1)
+    y_gpr, y_std = m.predict_y(X_plot)
+
+    ## generate 10 samples from posterior
+    tf.random.set_seed(1)  # for reproducibility
+    samples = m.predict_f_samples(X_plot, 10)  # shape (10, 100, 1)
+
+    plt.figure()
+    plt.title('GP fit for Gilgit (35.8884°N, 74.4584°E, 1500m)')
+    plt.errorbar(X_train + 1981, y_train, dy_train, fmt='r.', markersize=10, label='ERA5')
+    plt.plot(X_predictions + 1981, y_gpr[:, 0], 'b-', label='Prediction')
+    plt.fill_between(X_predictions + 1981, y_gpr[:, 0] - 1.9600 * y_std[:, 0], y_gpr[:, 0] + 1.9600 * y_std[:, 0],
+             alpha=.2, color='b', label='95% confidence interval')
+    #plt.plot(X_predictions + 1981, samples[:, :, 0].numpy().T, "C0", linewidth=0.5)
+    plt.legend()
+    plt.ylabel('Precipitation [mm/day]')
+    plt.xlabel('Year')
+    plt.show()
 
 
