134b1fa40c49456a215a6d84ada8e8c89df2d7b0
Kenza Tazi <kt2015@ic.ac.uk>
2020-05-28 21:38:19 +0100

    formatting

--- FileDownloader.py
+++ FileDownloader.py
@@ -29,13 +29,13 @@ def update_url_data(url, file, name):
     df_long = pd.melt(df_split, id_vars=[0], value_vars=np.arange(1,13), var_name='month', value_name=name)
     
     # Create a datetime column
-    df_long['date'] = df_long[0].astype(str) + '-' + df_long['month'].astype(str)
-    df_long['date'] = pd.to_datetime(df_long['date'], errors='coerce') 
+    df_long['time'] = df_long[0].astype(str) + '-' + df_long['month'].astype(str)
+    df_long['time'] = pd.to_datetime(df_long['time'], errors='coerce') 
     
     # Clean
     df_clean = df_long.dropna()
-    df_sorted = df_clean.sort_values(by=['date'])
-    df_final = df_sorted.set_index('date')
+    df_sorted = df_clean.sort_values(by=['time'])
+    df_final = df_sorted.set_index('time')
 
     return pd.DataFrame(df_final[name])
 
@@ -59,7 +59,7 @@ def update_cds_data(dataset_name='reanalysis-era5-single-levels-monthly-means',
     """
 
     now = datetime.datetime.now()
-    filename = dataset_name + '_' + product_type + '_' + now.strftime("%m-%Y")+'.nc'
+    filename = dataset_name + '_' + product_type + '_' + now.strftime("%m-%Y")+'.nc' # TODO include variables in pathname
 
     filepath = path + filename 
 
