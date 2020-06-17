62241f0fde4934a195099d61c34eb945898b102c
Kenza Tazi <kt2015@ic.ac.uk>
2020-06-17 14:45:08 +0100

    xarray arg + kfolding

--- DataPreparation.py
+++ DataPreparation.py
@@ -9,26 +9,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn import preprocessing
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 
 import FileDownloader as fd
 
 
 # Filepaths and URLs
-    
+  
 mask_filepath = '/Users/kenzatazi/Downloads/ERA5_Upper_Indus_mask.nc'
 
-nao_url = 'https://www.psl.noaa.gov/data/correlation/nao.data'
-n34_url =  'https://psl.noaa.gov/data/correlation/nina34.data'
-n4_url =  'https://psl.noaa.gov/data/correlation/nina4.data'
-
+'''
 tp_filepath = '/Users/kenzatazi/Downloads/era5_tp_monthly_1979-2019.nc'
 tp_ensemble_filepath ='/Users/kenzatazi/Downloads/adaptor.mars.internal-1587987521.7367163-18801-5-5284e7a8-222a-441b-822f-56a2c16614c2.nc'
 mpl_filepath = '/Users/kenzatazi/Downloads/era5_msl_monthly_1979-2019.nc'
+'''
 
+def download_data(mask_filepath, xarray=False, ensemble=False): # TODO include variables in pathname 
+    """ 
+    Downloads data for prepearation or analysis
 
-def download_data(mask_filepath, xarray=False): # TODO include variables in pathname 
-
-    """ Returns dataframe of data"""
+    Inputs
+        mask_filepath: string
+        xarray: boolean
+        ensemble: boolean 
+    
+    Returns 
+        df: DataFrame of data, or
+        ds: DataArray of data
+    """
 
     nao_url = 'https://www.psl.noaa.gov/data/correlation/nao.data'
     n34_url =  'https://psl.noaa.gov/data/correlation/nina34.data'
@@ -37,7 +46,10 @@ def download_data(mask_filepath, xarray=False): # TODO include variables in path
     path = '/Users/kenzatazi/Downloads/'
 
     now = datetime.datetime.now()
-    filename = 'combi_data' + '_' + now.strftime("%m-%Y")+'.csv' 
+    if ensemble == False:
+        filename = 'combi_data' + '_' + now.strftime("%m-%Y")+'.csv' 
+    else:
+        filename = 'combi_data_ensemble' + '_' + now.strftime("%m-%Y")+'.csv'
 
     filepath = path + filename
     print(filepath)
@@ -51,22 +63,35 @@ def download_data(mask_filepath, xarray=False): # TODO include variables in path
         ind_df = nao_df.join([n34_df, n4_df]).astype('float64')
 
         # Orography, humidity and precipitation
-        cds_filepath = fd.update_cds_data()
+        if ensemble == False:
+            cds_filepath = fd.update_cds_data()
+        else:
+            cds_filepath = fd.update_cds_data(product_type='monthly_averaged_ensemble_members')
+
         masked_da = apply_mask(cds_filepath, mask_filepath)
         multiindex_df = masked_da.to_dataframe()
         cds_df = multiindex_df.reset_index()
 
         # Combine
         df_combined = pd.merge_ordered(cds_df, ind_df, on='time')
-        df_combined.to_csv(filepath)
+        df_clean = df_combined.drop(columns= ['expver']).dropna()
+        df_clean['time'] = df_clean['time'].astype('int')
+        df_clean.to_csv(filepath)
 
-        return df_combined
+        if xarray == True:
+            df_multi = df_clean.set_index(['time', 'longitude', 'latitude'])
+            ds = df_multi.to_xarray()
+            return ds
+        else:    
+            return df_combined
     
     else:
         df = pd.read_csv(filepath)
 
         if xarray == True:
-            return df
+            df_multi = df.set_index(['time', 'longitude', 'latitude'])
+            ds = df_multi.to_xarray()
+            return ds
         else:    
             return df
 
@@ -112,7 +137,7 @@ def cumulative_monthly(da):
     return da * dim_mesh
 
 
-def point_data_prep(da): # TODO Link to CDS API, currently broken
+def point_data_prep(): # TODO Link to CDS API, currently broken
     """ 
     Outputs test and training data for total precipitation as a function of time from an ensemble of models for a single location
 
@@ -128,6 +153,9 @@ def point_data_prep(da): # TODO Link to CDS API, currently broken
         dy_test: testing standard deviation vector, numpy array
 
     """
+
+    da = download_data(mask_filepath, xarray=True, ensemble=True)
+    print(da)
     
     std_da = da.std(dim='number')
     mean_da = da.mean(dim='number')
@@ -177,43 +205,22 @@ def multivariate_data_prep(): # TODO generalise to ensemble data
         x_test: testing feature vector, numpy array
         y_test: testing output vector, numpy array
     """
-    # Front indices
-    nao_df = fd.update_url_data(nao_url, 'NAO')
-    n34_df = fd.update_url_data(n34_url, 'N34')
-    n4_df = fd.update_url_data(n4_url, 'N4')
-    ind_df = nao_df.join([n34_df, n4_df]).astype('float64')
-
-
-    # Orography, humidity and precipitation
-    cds_filepath = fd.update_cds_data(variables=['2m_dewpoint_temperature', 'angle_of_sub_gridscale_orography', 
-                                                'orography', 'slope_of_sub_gridscale_orography', 
-                                                'total_column_water_vapour', 'total_precipitation'])
-    masked_da = apply_mask(cds_filepath, mask_filepath)
-    gilgit = masked_da.interp(coords={'longitude':74.4584, 'latitude':35.8884 }, method='nearest')
+    da = download_data(mask_filepath, xarray=True)
+    gilgit = da.interp(coords={'longitude':74.4584, 'latitude':35.8884 }, method='nearest')
     multiindex_df = gilgit.to_dataframe()
-    cds_df = multiindex_df.reset_index()
-
-    # Combine
-    df_combined = pd.merge_ordered(cds_df, ind_df, on='time')  
-    df = df_combined.drop(columns=['expver', 'longitude', 'latitude'])
-    df['time'] = df['time'].astype('int')
-    #df_normalised = normalise(df)
-    df_clean = df.dropna()
+    df_clean = multiindex_df.reset_index()
 
     # Seperate y and x
     y = df_clean['tp'].values*1000
 
     x1 = df_clean.drop(columns=['tp'])
+    print(x1['time'])
     x1['time'] = (x1['time'] - x1['time'].min())/ (1e9*60*60*24*365)
     x = x1.values.reshape(-1, 9)
+
+    xtrain, xval, xtest, ytrain, yval, ytest = kfold_split(x, y)
     
-    x_train = x[0:400]
-    y_train = y[0:400]
-
-    x_test = x[400:-2]
-    y_test = y[400:-2]
-
-    return x_train, y_train, x_test, y_test
+    return xtrain, xval, xtest, ytrain, yval, ytest
 
 
 def gp_area_prep(mask_filepath):
@@ -246,13 +253,9 @@ def gp_area_prep(mask_filepath):
     x1['time'] = (x1['time'] - x1['time'].min())/ (1e9*60*60*24*365)
     x = x1.values.reshape(-1, 11)
     
-    x_train = x[0:40000]
-    y_train = y[0:40000]
-
-    x_test = x[40000:50000]
-    y_test = y[40000:50000]
-
-    return x_train, y_train, x_test, y_test
+    xtrain, xval, xtest, ytrain, yval, ytest = kfold_split(x, y)
+    
+    return xtrain, xval, xtest, ytrain, yval, ytest
 
 
 def normalise(df):
@@ -265,6 +268,41 @@ def normalise(df):
     return df
 
 
-if __name__ == "__main__":
-    da = apply_mask(tp_ensemble_filepath, mask_filepath)
+def kfold_split(x, y, dy=None, folds=5):
+    """ 
+    Split values into training, validation and test sets.
+    The training and validation set are prepared for k folding.
+
+    Inputs
+        x: array of features
+        y: array of target values
+        dy: array of uncertainty on target values
+        folds: number of kfolds
+
+    Outputs
+        xtrain: feature training set
+        xvalidation: feature validation set
+        xtest: feature test set
+
+        ytrain: target training set
+        yvalidation: target validation set
+        ytest: target test set
+
+        dy_train: uncertainty training set
+        dy_validation: uncertainty validation set
+        dy_test: uncertainty test set
+    """
+    # Remove test set without shuffling
+    xtr, xtest, ytr, ytest = train_test_split(x, y, test_size=0.15)
+    
+    kf = KFold(n_splits=folds)
+    for train_index, test_index in kf.split(xtr):
+        xtrain, xval = xtr[train_index], xtr[test_index]
+        ytrain, yval = ytr[train_index], ytr[test_index]
+
+    return  xtrain, xval, xtest, ytrain, yval, ytest
+
+
+
+    
 
