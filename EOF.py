1e974bb7cc0e0bc0b18af00f4600791a5aac975d
Kenza Tazi <kt2015@ic.ac.uk>
2020-07-07 16:29:14 +0100

    fixed dates

--- EOF.py
+++ EOF.py
@@ -25,12 +25,12 @@ for y in range(40):
     for m in np.arange(1,13):
         
         ## Select subperiod
-        start_date = str(1979+y) + str(m) + '01T12:00:00'
+        start_date = str(1979+y) + '-' + str(m) + '-01T12:00:00'
         
         if m == 12:
-            end_date = str(1979+y+1 )+ str(1) +'01T12:00:00'
+            end_date = str(1979+y+1) + '-' +  str(1) +'-01T12:00:00'
         else:
-            end_date = str(1979+y) + str(m+1) +'01T12:00:00'
+            end_date = str(1979+y) + '-' + str(m+1) +'-01T12:00:00'
 
         z200_month = z200.sel(time=slice(start_date, end_date))
 
