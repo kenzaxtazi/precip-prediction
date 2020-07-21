9510b828d43b2095639230dec4f697d72f2bec9b
Kenza Tazi <kt2015@ic.ac.uk>
2020-07-21 16:55:54 +0100

    mean + eof+ ind+ cds_downloader

--- DataDownloader.py
+++ DataDownloader.py
@@ -25,20 +25,17 @@ def download_data(mask_filepath, xarray=False, ensemble=False, all_var=False):
     Inputs
         mask_filepath: string
         xarray: boolean
-        ensemble: boolean 
+        ensemble: boolean
+        all_var: boolean
     
     Returns 
         df: DataFrame of data, or
         ds: DataArray of data
     """
 
-    nao_url = 'https://www.psl.noaa.gov/data/correlation/nao.data'
-    n34_url =  'https://psl.noaa.gov/data/correlation/nina34.data'
-    n4_url =  'https://psl.noaa.gov/data/correlation/nina4.data'
-
     path = 'Data/'
-
     now = datetime.datetime.now()
+
     if ensemble == False:
         filename = 'combi_data' + '_' + now.strftime("%m-%Y")+'.csv'
     if all_var == True:
@@ -51,58 +48,22 @@ def download_data(mask_filepath, xarray=False, ensemble=False, all_var=False):
 
     if not os.path.exists(filepath):
 
-        # Indices
-
-        n34_df = fd.update_url_data(n34_url, 'N34')
-
-        if all_var == False:    
-            ind_df = n34_df.astype('float64')
-        else:
-            nao_df = fd.update_url_data(nao_url, 'NAO')
-            n4_df = fd.update_url_data(n4_url, 'N4')
-            ind_df = n34_df.join([nao_df, n4_df])
+        # Orography, humidity, precipitation and indices
+        cds_df = cds_downloader(mask_filepath, ensemble=False)
+        ind_df = indice_downloader(all_var = all_var)
+        df_combined = pd.merge_ordered(cds_df, ind_df, on='time', suffixes=("", "_y"))
         
+        # Other variables not used in the GP
         if all_var == True:
-            # Temperature
-            temp_filepath = fd.update_cds_monthly_data(variables=['2m_temperature'], area=[40, 65, 20, 85], qualifier='temp')
-            temp_df = mean_formatter(temp_filepath)
+            mean_df = mean_downloader()
+            uib_eofs_df = eof_downloader(mask_filepath, all_var = all_var)
+    
+            # Combine
+            df_combined = pd.merge_ordered(df_combined, mean_df, on='time')
+            df_combined = pd.merge_ordered(df_combined, uib_eofs_df, on=['time', 'latitude', 'longitude'])
 
-            # EOF UIB
-            eof_da = apply_mask('Data/regional_z200_EOF2.nc', mask_filepath)
-            eof_ds = eof_da.EOF
-            eof2 = eof_ds.assign_coords(time=(eof_ds.time.astype('datetime64'))) 
-            eof_multiindex_df = eof2.to_dataframe()
-            eof_df = eof_multiindex_df.reset_index()
-            eof_df['time'] -= np.timedelta64(12,'h')
-
-            # eof200b_df = mean_formatter('Data/regional_z200_EOF2.nc', coords =)
-            eof200c_df = mean_formatter('Data/regional_z200_EOF2.nc', coords = [40, 60, 35,70])
-            eof200c_df['time'] -= np.timedelta64(12,'h')
-
-            eof200b_df = mean_formatter('Data/regional_z200_EOF2.nc', coords = [16, 83, 19, 93])
-            eof200b_df['time'] -= np.timedelta64(12,'h')
-
-        # Orography, humidity and precipitation
-        if ensemble == False:
-            cds_filepath = fd.update_cds_monthly_data()
-        else:
-            cds_filepath = fd.update_cds_monthly_data(product_type='monthly_averaged_ensemble_members')
-
-        masked_da = apply_mask(cds_filepath, mask_filepath)
-        multiindex_df = masked_da.to_dataframe()
-        cds_df = multiindex_df.reset_index()
-
-        # Combine
-        df_combined = pd.merge_ordered(cds_df, ind_df, on='time')
-
-        if all_var == True:
-            df_combined = pd.merge_ordered(df_combined, temp_df, on='time')
-            df_combined = pd.merge_ordered(df_combined, eof_df, on='time')
-            #df_combined4 = pd.merge_ordered(df_combined3, eof200a_df, on='time')
-            df_combined = pd.merge_ordered(df_combined, eof200b_df, on='time')
-            df_combined = pd.merge_ordered(df_combined, eof200c_df, on='time')
-        
-        df_clean = df_combined.dropna() #columns=['expver_x', 'expver_y']
+        # Clean   
+        df_clean = df_combined.dropna()
         df_clean['time'] = df_clean['time'].astype('int')
         df_clean = df_clean.astype('float64')
         df_clean.to_csv(filepath)
@@ -158,20 +119,114 @@ def apply_mask(data_filepath, mask_filepath):
     return UIB
 
 
-def mean_formatter(filepath, coords=None):
-    
-    da = xr.open_dataset(filepath)
-    
-    if 'expver' in list(da.dims):
-        da = da.sel(expver=1)
-    
-    if coords != None:
-        da = da.sel(latitude=slice(coords[0], coords[2]), longitude=slice(coords[1], coords[3]))
+def mean_downloader():
 
-    mean_da = da.mean(dim=['longitude', 'latitude'], skipna=True) 
-    eof = mean_da.assign_coords(time=(mean_da.time.astype('datetime64'))) 
-    multiindex_df = eof.to_dataframe()
-    df = multiindex_df.reset_index()
+    def mean_formatter(filepath, coords=None, name=None):
+        """ Returns dataframe averaged data over a optionally given area """
 
-    return df
+        da = xr.open_dataset(filepath)
+        
+        if 'expver' in list(da.dims):
+            da = da.sel(expver=1)
+            da = da.drop(['expver'])
+        
+        if coords != None:
+            da = da.sel(latitude=slice(coords[0], coords[2]), longitude=slice(coords[1], coords[3]))
 
+        mean_da = da.mean(dim=['longitude', 'latitude'], skipna=True) 
+        clean_da = mean_da.assign_coords(time=(mean_da.time.astype('datetime64'))) 
+        multiindex_df = clean_da.to_dataframe()
+        df = multiindex_df #.reset_index()
+        if name != None:
+            df.rename(columns = {'EOF': name}, inplace=True)
+
+        return df
+
+    # Temperature
+    temp_filepath = fd.update_cds_monthly_data(variables=['2m_temperature'], area=[40, 65, 20, 85], qualifier='temp')
+    temp_df = mean_formatter(temp_filepath)
+
+    # EOFs for 200hPa
+    eof1_z200_c = mean_formatter('Data/regional_z200_EOF1.nc', coords = [40, 60, 35,70], name='eof1_z200_c')
+    eof1_z200_b = mean_formatter('Data/regional_z200_EOF1.nc', coords = [19, 83, 16, 93], name='eof1_z200_b')
+    eof2_z200_c = mean_formatter('Data/regional_z200_EOF2.nc', coords = [40, 60, 35,70], name='eof2_z200_c')
+    eof2_z200_b = mean_formatter('Data/regional_z200_EOF2.nc', coords = [19, 83, 16, 93], name='eof2_z200_b')
+
+    # EOFs for 500hPa
+    eof1_z500_c = mean_formatter('Data/regional_z500_EOF1.nc', coords = [40, 60, 35,70], name='eof1_z500_c')
+    eof1_z500_b = mean_formatter('Data/regional_z500_EOF1.nc', coords = [19, 83, 16, 93], name='eof1_z500_b')
+    eof2_z500_c = mean_formatter('Data/regional_z500_EOF2.nc', coords = [40, 60, 35,70], name='eof2_z500_c')
+    eof2_z500_b = mean_formatter('Data/regional_z500_EOF2.nc', coords = [19, 83, 16, 93], name='eof2_z500_b')
+
+    # EOFs for 850hPa
+    eof1_z850_c = mean_formatter('Data/regional_z850_EOF1.nc', coords = [40, 60, 35,70], name='eof1_z850_c')
+    eof1_z850_b = mean_formatter('Data/regional_z850_EOF1.nc', coords = [19, 83, 16, 93], name='eof1_z850_b')
+    eof2_z850_c = mean_formatter('Data/regional_z850_EOF2.nc', coords = [40, 60, 35,70], name='eof2_z850_c')
+    eof2_z850_b = mean_formatter('Data/regional_z850_EOF2.nc', coords = [19, 83, 16, 93], name='eof2_z850_b')
+
+    eof_df = pd.concat([eof1_z200_b, eof1_z200_c, eof2_z200_b, eof2_z200_c, eof1_z500_b, eof1_z500_c, 
+                        eof2_z500_b, eof2_z500_c, eof1_z850_b, eof1_z850_c, eof2_z850_b, eof2_z850_c], axis=1)
+
+    mean_df = pd.merge_ordered(temp_df, eof_df, on='time')
+
+    return mean_df
+
+
+def eof_downloader(mask_filepath, all_var=False):
+
+    def eof_formatter(filepath, mask_filepath, name=None):
+        """ Returns DataFrame of EOF over UIB  """
+        eof_da = apply_mask(filepath, mask_filepath)
+        eof_ds = eof_da.EOF
+        eof2 = eof_ds.assign_coords(time=(eof_ds.time.astype('datetime64'))) 
+        eof_multiindex_df = eof2.to_dataframe()
+        eof_df = eof_multiindex_df.dropna()
+        eof_df.rename(columns = {'EOF': name}, inplace = True)
+        return eof_df
+
+    # EOF UIB
+    eof1_z200_u = eof_formatter('Data/regional_z200_EOF1.nc', mask_filepath, name='eof_z200_u')
+    eof1_z500_u = eof_formatter('Data/regional_z500_EOF1.nc', mask_filepath, name='eof_z500_u')
+    eof1_z850_u = eof_formatter('Data/regional_z850_EOF1.nc', mask_filepath, name='eof_z850_u')
+
+    eof2_z200_u = eof_formatter('Data/regional_z200_EOF2.nc', mask_filepath, name='eof2_z200_u')
+    eof2_z500_u = eof_formatter('Data/regional_z500_EOF2.nc', mask_filepath, name='eof_z500_u')
+    eof2_z850_u = eof_formatter('Data/regional_z850_EOF2.nc', mask_filepath, name='eof_z850_u')
+
+    uib_eofs = pd.concat([eof1_z200_u, eof2_z200_u, eof1_z500_u, eof2_z500_u, eof1_z850_u, eof2_z850_u], axis=1)
+
+    return uib_eofs
+
+
+def indice_downloader(all_var=False):
+    """ Return indice Dataframe """
+
+    nao_url = 'https://www.psl.noaa.gov/data/correlation/nao.data'
+    n34_url =  'https://psl.noaa.gov/data/correlation/nina34.data'
+    n4_url =  'https://psl.noaa.gov/data/correlation/nina4.data'
+
+    n34_df = fd.update_url_data(n34_url, 'N34')
+
+    if all_var == False:    
+        ind_df = n34_df.astype('float64')
+    else:
+        nao_df = fd.update_url_data(nao_url, 'NAO')
+        n4_df = fd.update_url_data(n4_url, 'N4')
+        ind_df = n34_df.join([nao_df, n4_df])
+    
+    return ind_df
+
+
+def cds_downloader(mask_filepath, ensemble=False):
+    """ Return CDS Dataframe """
+
+    if ensemble == False:
+        cds_filepath = fd.update_cds_monthly_data()
+    else:
+        cds_filepath = fd.update_cds_monthly_data(product_type='monthly_averaged_ensemble_members')
+
+    masked_da = apply_mask(cds_filepath, mask_filepath)
+    multiindex_df = masked_da.to_dataframe()
+    cds_df = multiindex_df.reset_index()
+
+    return cds_df
