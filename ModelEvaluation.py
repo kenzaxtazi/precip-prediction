ffe5950cc0dc561c48456112790fb8e5e6860dcd
Kenza Tazi <kt2015@ic.ac.uk>
2020-07-23 16:15:44 +0100

    exeptions for unsolvable matrix

--- ModelEvaluation.py
+++ ModelEvaluation.py
@@ -78,15 +78,28 @@ def uib_evaluation(average=False):
     sample_list = [1000, 3000, 5000, 7000, 9000, 11000, 14000]
 
     for i in tqdm(sample_list):
-        xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i, EDA_average=average)
-        m = gpm.multi_gp(xtrain, xval, ytrain, yval)
+        try:
+            xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i, EDA_average=average)
+            m = gpm.multi_gp(xtrain, xval, ytrain, yval)
 
-        training_R2 = me.R2(m, xtrain, ytrain)
-        training_RMSE = me.RMSE(m, xtrain, ytrain)
-        val_R2 = me.R2(m, xval, yval)
-        val_RMSE = me.RMSE(m, xval, yval)
+            training_R2 = me.R2(m, xtrain, ytrain)
+            training_RMSE = me.RMSE(m, xtrain, ytrain)
+            val_R2 = me.R2(m, xval, yval)
+            val_RMSE = me.RMSE(m, xval, yval)
 
-        metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
+            metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
+
+        except Exception:
+            print(i+100)
+            xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i+100, EDA_average=average)
+            m = gpm.multi_gp(xtrain, xval, ytrain, yval)
+
+            training_R2 = me.R2(m, xtrain, ytrain)
+            training_RMSE = me.RMSE(m, xtrain, ytrain)
+            val_R2 = me.R2(m, xval, yval)
+            val_RMSE = me.RMSE(m, xval, yval)
+
+            metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
 
     df = pd.DataFrame(metric_list, columns=['samples', 'training_R2', 'training_RMSE', 'val_R2', 'val_RMSE'])
     df.to_csv('uib-eval-2020-07-22.csv')
@@ -101,17 +114,63 @@ def cluster_evaluation(cluster_mask):
     sample_list = [1000, 3000, 5000, 7000, 9000, 11000, 14000]
 
     for i in tqdm(sample_list):
-        xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i, cluster_mask=cluster_mask)
-        m = gpm.multi_gp(xtrain, xval, ytrain, yval)
+        try:
+            xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i, cluster_mask=cluster_mask)
+            m = gpm.multi_gp(xtrain, xval, ytrain, yval)
 
-        training_R2 = me.R2(m, xtrain, ytrain)
-        training_RMSE = me.RMSE(m, xtrain, ytrain)
-        val_R2 = me.R2(m, xval, yval)
-        val_RMSE = me.RMSE(m, xval, yval)
+            training_R2 = me.R2(m, xtrain, ytrain)
+            training_RMSE = me.RMSE(m, xtrain, ytrain)
+            val_R2 = me.R2(m, xval, yval)
+            val_RMSE = me.RMSE(m, xval, yval)
 
-        metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
+            metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
+        
+        except Exception:
+            print(i+100)
+            xtrain, xval, xtest, ytrain, yval, ytest = dp.random_multivariate_data_prep(length=i+100, cluster_mask=cluster_mask)
+            m = gpm.multi_gp(xtrain, xval, ytrain, yval)
+
+            training_R2 = me.R2(m, xtrain, ytrain)
+            training_RMSE = me.RMSE(m, xtrain, ytrain)
+            val_R2 = me.R2(m, xval, yval)
+            val_RMSE = me.RMSE(m, xval, yval)
+
+            metric_list.append([i, training_R2, training_RMSE, val_R2, val_RMSE])
+
 
     df = pd.DataFrame(metric_list, columns=['samples', 'training_R2', 'training_RMSE', 'val_R2', 'val_RMSE'])
     df.to_csv(name + '-eval-2020-07-22.csv')
 
 
+def sampled_points(filepath1, label1, filepath2=None, label2=None, filepath3=None, label3=None):
+    """ Returns plots of R2 and RMSE as function of sampled data points """
+    
+    df1 = pd.read_csv(filepath1)
+
+    if filepath2 != None:
+        df2 = pd.read_csv(filepath2)
+
+    if filepath3 != None:
+        df3 = pd.read_csv(filepath3)
+    
+    plt.figure('R2')
+    plt.scatter(df1['samples'].values, df1['R2_val'].values, c='b', label=label1)
+    if filepath2 != None:
+        plt.scatter(df2['samples'].values, df2['R2_val'].values, c='o', label=label2)
+    if filepath3 != None:
+        plt.scatter(df3['samples'].values, df3['R2_val'].values, c='g', label=label3)
+
+    plt.figure('RMSE')
+    plt.scatter(df1['samples'].values, df1['RMSE_val'].values, c='b', label=label1)
+    if filepath2 != None:
+        plt.scatter(df2['samples'].values, df2['RMSE_val'].values, c='o', label=label2)
+    if filepath3 != None:
+        plt.scatter(df3['samples'].values, df3['RMSE_val'].values, c='g', label=label3)
+    
+    plt.show()
+
+
+def weigted_av_perf():
+    """ Returns weighted average performance for the three clusters """
+
+    print 
