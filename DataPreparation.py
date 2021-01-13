394f46a6db74b8459f19da553c32a0ee167cef51
Kenza Tazi <kt2015@ic.ac.uk>
2021-01-13 22:16:28 +0000

    slm_multivariate_data_prep()

--- DataPreparation.py
+++ DataPreparation.py
@@ -38,7 +38,7 @@ def cumulative_monthly(da):
     return da * dim_mesh
 
 
-def multivariate_data_prep(number=None, EDA_average=False, coords=None):
+def slm_multivariate_data_prep(number=None, EDA_average=False, coords=None):
     """
     Outputs test, validation and training data for total precipitation as a function of time, 2m dewpoint temperature,
     angle of sub-gridscale orography, orography, slope of sub-gridscale orography, total column water vapour,
@@ -82,6 +82,67 @@ def multivariate_data_prep(number=None, EDA_average=False, coords=None):
     )  # to years
     df["tp"] = log_transform(df["tp"])
     df = df[["time", "d2m", "tcwv", "N34", "tp"]] #format order
+
+    # Keep first of 70% for training
+    train_df = df[df["time"] < df["time"].max() * 0.7]
+    xtrain = train_df.drop(columns=["tp"]).values
+    ytrain = train_df["tp"].values
+
+    # Last 30% for evaluation
+    eval_df = df[df["time"] > df["time"].max() * 0.7]
+    x_eval = eval_df.drop(columns=["tp"]).values
+    y_eval = eval_df["tp"].values
+
+    # Training and validation data
+    xval, xtest, yval, ytest = train_test_split(
+        x_eval, y_eval, test_size=0.3333, shuffle=False
+    )
+
+    return xtrain, xval, xtest, ytrain, yval, ytest
+
+
+def multivariate_data_prep(number=None, EDA_average=False, coords=None):
+    """
+    Outputs test, validation and training data for total precipitation as a function of time, 2m dewpoint temperature,
+    angle of sub-gridscale orography, orography, slope of sub-gridscale orography, total column water vapour,
+    Nino 3.4, Nino 4 and NAO index for a single point.
+
+    Inputs
+        None
+
+    Outputs
+        x_train: training feature vector, numpy array
+        y_train: training output vector, numpy array
+        x_test: testing feature vector, numpy array
+        y_test: testing output vector, numpy array
+    """
+    if number != None:
+        da_ensemble = dd.download_data(mask_filepath, xarray=True, ensemble=True)
+        da = da_ensemble.sel(number=number).drop("number")
+    if EDA_average == True:
+        da_ensemble = dd.download_data(mask_filepath, xarray=True, ensemble=True)
+        da = da_ensemble.mean(dim="number")
+    else:
+        da = dd.download_data(mask_filepath, xarray=True)
+
+    if coords == None:
+        multiindex_df = da.to_dataframe()
+        df_clean = multiindex_df.dropna().reset_index()
+        df_location = sa.random_location_sampler(df_clean)
+
+    else:
+        da_location = da.interp(
+            coords={"latitude": coords[0], "longitude": coords[1]}, method="nearest"
+        )
+        multiindex_df = da_location.to_dataframe()
+        df = multiindex_df.dropna().reset_index()
+
+    df["time"] = df["time"].astype("int")
+    df["time"] = (df["time"] - df["time"].min()) / (
+        1e9 * 60 * 60 * 24 * 365
+    )  # to years
+    df["tp"] = log_transform(df["tp"])
+    df = df[["time", "latitude", "longitude", "slor", "anor", "z", "d2m", "tcwv", "N34", "tp"]] #format order
 
     # Keep first of 70% for training
     train_df = df[df["time"] < df["time"].max() * 0.7]
