5e3a12c8d985a8495abcb0fa6c6eb913dd5b4e0c
Kenza Tazi <kt2015@ic.ac.uk>
2021-01-25 17:14:20 +0000

    Renamed to Benchmarking

--- Benchmarking.py
+++ Benchmarking.py
@@ -7,26 +7,37 @@ import xarray as xr
 import cftime
 import datetime
 import glob
+import seaborn as sns
+
+from scipy import stats
 
 import DataDownloader as dd
 import DataPreparation as dp
 import GPModels as gpm
+import Correlation as corr
+
+model_filepath = 'Models/model_2021-01-01/08/21-22-35-56'
 
 
 def collect_ERA5():
     """ Downloads data from ERA5 """
     mask_filepath = "Data/ERA5_Upper_Indus_mask.nc"
-    era5_ds= dd.download_data(mask_filepath, xarray=True)
+    era5_ds= dd.download_data(mask_filepath, xarray=True) # in m/day
+    era5_ds['tp'] *= 1000  # to mm/day
     era5_ds = era5_ds.assign_attrs(plot_legend="ERA5")
+    era5_ds['time'] = standardised_time(era5_ds)
+    era5_ds = era5_ds.rename({'latitude': 'lat', 'longitude': 'lon'})
     return era5_ds
 
 def collect_CMIP5():
     """ Downloads data from CMIP5 """
     cmip_59_84_ds = xr.open_dataset("/Users/kenzatazi/Downloads/pr_Amon_HadCM3_historical_r1i1p1_195912-198411.nc")
     cmip_84_05_ds = xr.open_dataset("/Users/kenzatazi/Downloads/pr_Amon_HadCM3_historical_r1i1p1_198412-200512.nc")
-    cmip_ds = cmip_84_05_ds.merge(cmip_59_84_ds)
+    cmip_ds = cmip_84_05_ds.merge(cmip_59_84_ds)  # in kg/m2/s
     cmip_ds = cmip_ds.assign_attrs(plot_legend="HadCM3 historical")
-    cmip_ds = cmip_ds.rename_vars({'pr': 'tp'})
+    cmip_ds = cmip_ds.rename({'pr': 'tp'})
+    cmip_ds['tp'] *= 60 * 60 * 24  # to mm/day
+    cmip_ds['time'] = standardised_time(cmip_ds)
     return cmip_ds
 
 def collect_CORDEX():
@@ -37,23 +48,30 @@ def collect_CORDEX():
     cordex_02_11_ds = xr.open_dataset("/Users/kenzatazi/Downloads/pr_EAS-44i_ECMWF-ERAINT_evaluation_r1i1p1_MOHC-HadRM3P_v1_mon_201101-201111.nc")
     cordex_90_00_ds = cordex_90_ds.merge(cordex_91_00_ds)
     cordex_01_11_ds= cordex_01_ds.merge(cordex_02_11_ds)
-    cordex_ds = cordex_01_11_ds.merge(cordex_90_00_ds)
-    cordex_ds = cordex_ds.assign_attrs(plot_legend="CORDEX EA - MOHC-HadRM3P historical")   
+    cordex_ds = cordex_01_11_ds.merge(cordex_90_00_ds)  # in kg/m2/s
+    
+    cordex_ds = cordex_ds.assign_attrs(plot_legend="CORDEX EA - MOHC-HadRM3P historical")
     cordex_ds = cordex_ds.rename_vars({'pr': 'tp'})
+    cordex_ds['tp'] *= 60 * 60 * 24   # to mm/day
+    cordex_ds['time'] = standardised_time(cordex_ds)
+
     return cordex_ds
 
 def collect_APHRO():
     """ Downloads data from APHRODITE model"""
     aphro_ds = xr.merge([xr.open_dataset(f) for f in glob.glob('/Users/kenzatazi/Downloads/APHRO_MA_025deg_V1101.1951-2007.gz/*')])
-    aphro_ds = aphro_ds.assign_attrs(plot_legend="APHRODITE")   
-    aphro_ds = aphro_ds.rename_vars({'pr': 'tp'})
+    aphro_ds = aphro_ds.assign_attrs(plot_legend="APHRODITE") # in mm/day   
+    aphro_ds = aphro_ds.rename_vars({'precip': 'tp'})
+    aphro_ds['time'] = standardised_time(aphro_ds)
     return aphro_ds
 
 def collect_CRU():
     """ Downloads data from CRU model"""
     cru_ds = xr.open_dataset("/Users/kenzatazi/Downloads/cru_ts4.04.1901.2019.pre.dat.nc")
-    cru_ds = cru_ds.assign_attrs(plot_legend="CRU")
+    cru_ds = cru_ds.assign_attrs(plot_legend="CRU") # in mm/month
     cru_ds = cru_ds.rename_vars({'pre': 'tp'})
+    cru_ds['tp'] /= 30.437  #TODO apply proper function to get mm/day
+    cru_ds['time'] = standardised_time(cru_ds)
     return cru_ds
 
 def standardised_time(dataset):
@@ -69,12 +87,14 @@ def standardised_time(dataset):
 
 def select_coords(lat, lon, dataset):
     """ Interpolate dataset at given coordinates """
-    try:
-        timeseries = dataset.interp(coords={"longitude": lon, "latitude": lat}, method="nearest")
-    except Exception:
-        timeseries = dataset.interp(coords={"lon": lon, "lat": lat}, method="nearest")
+    timeseries = dataset.interp(coords={"lon": lon, "lat": lat}, method="nearest")
+    timeseries = timeseries.sel(time= slice(1990, 2005))
     return timeseries
+        
 
+def select_basin(basin_name, dataset):  # TODO
+    basin_data = dd.apply_mask(dataset)
+    return basin_data
 
 def model_prep(model_filepath, lat, lon):
     """ Prepares model outputs for comparison """
@@ -83,7 +103,7 @@ def model_prep(model_filepath, lat, lon):
     xtrain, xval, _, _, _, _ = dp.multivariate_data_prep(coords=[lat,lon])
     
     xtr = np.concatenate((xtrain, xval), axis=0)
-    y_gpr, y_std = model.predict_y(xval)
+    y_gpr, y_std = model.predict_y(xtr)
  
     # to mm/day
     y_gpr_t = dp.inverse_log_transform(y_gpr) * 1000
@@ -92,23 +112,36 @@ def model_prep(model_filepath, lat, lon):
     return xtr, y_gpr_t, y_std_t
 
 
-def timeseries_comparison(timeseries, xtr, y_gpr_t, y_std_t):
+def timeseries_plot(timeseries, xtr, y_gpr_t, y_std_t):
     """ Plot timeseries of model outputs """
-    
     plt.figure()
-
     for ts in timeseries:
-        time = standardised_time(ts)
-        plt.plot(time, ts.pr.values, label=ts.plot_legend)
-
+        plt.plot(ts.time.values, ts.tp.values, label=ts.plot_legend)
     plt.fill_between(xtr[:, 0] + 1979, y_gpr_t[:, 0] - 1.9600 * y_std_t[:, 0], y_gpr_t[:, 0] + 1.9600 * y_std_t[:, 0], 
-                        alpha=0.5, color="lightblue", label="95% confidence interval")
-    plt.plot(xtr[:, 0] + 1979, y_gpr_t, color="C0", linestyle="-", label="Prediction")
-
+                        alpha=0.5, color="lightblue") #label="95% confidence interval")
+    plt.plot(xtr[:, 0] + 1979, y_gpr_t, linestyle="-", label="Prediction")
     plt.xlabel('Time')
     plt.ylabel('Precipitation mm/day')
     plt.legend()
     plt.show()
+
+
+
+def dataset_stats(datasets, xtr, y_gpr_t, y_std_t):
+    """ Print mean, standard deviations and slope for datasets """
+
+    for ds in datasets:
+        slope, intercept, r_value, p_value, std_err = stats.linregress(ds.time.values, ds.tp.values)
+        print(ds.plot_legend)
+        print('mean = ', np.mean(ds.tp.values),'mm/day')
+        print('std = ', np.std(ds.tp.values), 'mm/day')
+        print('slope = ', slope, 'mm/day/month')
+    
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xtr[131:313, 0], y_gpr_t[131:313, 0]) # btw 1990 and 2005
+    print("model")
+    print('mean = ', np.mean(y_gpr_t),'mm/day')
+    print('std = ', np.std(y_gpr_t), 'mm/day')
+    print('slope = ', slope, 'mm/day/month')
 
 
 def pdf_comparison(timeseries, xtr, y_gpr_t, y_std_t):
@@ -141,7 +174,9 @@ def trend_comparison(model_filepath, lat, lon):
 
     xtr, y_gpr_t, y_std_t = model_prep(model_filepath, lat, lon)
 
-    timeseries_comparison(timeseries, xtr, y_gpr_t, y_std_t)
+    timeseries_plot(timeseries, xtr, y_gpr_t, y_std_t)
+    dataset_stats(timeseries, xtr, y_gpr_t, y_std_t)
+    corr.dataset_correlation(timeseries)
 
 
 
