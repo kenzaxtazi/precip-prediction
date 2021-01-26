1217a9d039648726ddb4b265a5d5c2b6e38accc5
Kenza Tazi <kt2015@ic.ac.uk>
2021-01-26 17:06:52 +0000

    funcs updated for new data preproc. > now working

--- PDF.py
+++ PDF.py
@@ -34,7 +34,7 @@ def monthly_PDF(data_filepath, mask_filepath, variable="tp", longname=""):
 
     grouped_dfs = []
 
-    for m in month_labels:
+    for m in np.arange(1,13):
         month_df = reduced_df[reduced_df["time"] == m]
         grouped_dfs.append(month_df[variable])
 
@@ -91,7 +91,7 @@ def monthly_PDF(data_filepath, mask_filepath, variable="tp", longname=""):
     plt.show()
 
 
-def pdf_comparison(timeseries, xtr, y_gpr_t, y_std_t):
+def benchmarking_plot(timeseries, y_gpr_t):
     """ Plot probability distribution of model outputs """
 
     dataframes = []
@@ -99,39 +99,43 @@ def pdf_comparison(timeseries, xtr, y_gpr_t, y_std_t):
     for ts in timeseries:
         df1 = ts.tp.to_dataframe(name=ts.plot_legend)
         df2 = df1.reset_index()
-        df3 = df2.drop(["lon", "lat"], axis=1)
-        df3["time"] = (df3["time"] - np.floor(df3["time"])) * 12
+        df3 = df2.drop(["time", "lon", "lat"], axis=1)
         dataframes.append(df3)
 
-    
     combined_df = dataframes[0].join(dataframes[1:])
-    combined_df["model"] = y_gpr_t[132:312, 0]   
+    combined_df["model"] = y_gpr_t[132:312, 0]
+    
+    time_ds = timeseries[0].time.to_dataframe(name='time')
+    months_float = np.ceil((time_ds["time"] - np.floor(time_ds["time"])) * 12)
+    df =  combined_df.copy()
+    df["time"] = months_float.values
+    df["time"] = df["time"].astype(int)
 
     grouped_dfs = []
 
-    for m in month_labels:
-        month_df = combined_df[combined_df['time'] == m]
+    for m in range(1, 13):
+        month_df = df[df['time'] == m]
+        month_df = month_df.drop(['time'], axis=1)
         grouped_dfs.append(month_df)
 
     fig, axs = plt.subplots(3, 4, sharex=True, sharey=True)
-
     for i in range(12):
         x = (i) % 3
         y = int(i / 3)
-        axs[x, y].hist(grouped_dfs[i], density=True)
+        for b in list(combined_df):
+            axs[x, y].hist((grouped_dfs[i])[b], density=True, label=b) #, bins=np.arange(20),alpha=.5)
         axs[x, y].set_title(month_dict[i])
         axs[x, y].set_title(month_dict[i])
         axs[x, y].xaxis.set_tick_params(which="both", labelbottom=True)
         axs[x, y].yaxis.set_tick_params(which="both", labelbottom=True)
-        axs[x, y].set_xlabel("Precipitation (mm/day")
+        axs[x, y].set_xlabel("Precipitation (mm/day)")
         axs[x, y].set_ylabel("Probability density")
         axs[x, y].axvline(
-            np.percentile(grouped_dfs[i], 95),
+            np.percentile((grouped_dfs[i])['ERA5'], 95),
             color="k",
             linestyle="dashed",
             linewidth=1,
-            label="95th percentile",
+            label="ERA5 95th percentile",
         )
-
-    plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.30))
+    plt.legend(loc="upper right")    
     plt.show()
