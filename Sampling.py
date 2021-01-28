518a9e6b473b4d000cd5b22b26c417e05f3376b3
Kenza Tazi <kt2015@ic.ac.uk>
2021-01-28 18:31:42 +0000

    lat, lon dim names

--- Sampling.py
+++ Sampling.py
@@ -16,40 +16,40 @@ mask_filepath = "Data/ERA5_Upper_Indus_mask.nc"
 def random_location_sampler(df):
     """ Returns DataFrame of random location, apply to clean df only """
 
-    df_squished = df[["latitude", "longitude"]].reset_index()
+    df_squished = df[["lat", "lon"]].reset_index()
     df_s_reset = df_squished.drop_duplicates()
     i = np.random.randint(len(df_s_reset), size=1)
 
     df_location = df_s_reset.iloc[i]
-    lat = df_location["latitude"].values[0]
-    lon = df_location["longitude"].values[0]
+    lat = df_location["lat"].values[0]
+    lon = df_location["lon"].values[0]
     print("lat=" + str(lat) + ", lon=" + str(lon))
 
-    df1 = df[df["latitude"] == lat]
-    df2 = df1[df1["longitude"] == lon]
+    df1 = df[df["lat"] == lat]
+    df2 = df1[df1["lon"] == lon]
 
     return df2
 
 
-def random_location_generator(N=50, UIB=False):
+def random_location_generator(location, N=50):
     """ Returns DataFrame of random location, apply to clean df only """
 
     coord_list = []
 
-    df = dd.download_data(mask_filepath)
-    df_squished = df[["latitude", "longitude"]].reset_index()
-    df_s_reset = df_squished.drop_duplicates(subset=["latitude", "longitude"])
+    df = dd.download_data(location)
+    df_squished = df[["lat", "lon"]].reset_index()
+    df_s_reset = df_squished.drop_duplicates(subset=["lat", "lon"])
 
     if UIB == True:
-        coord_list = df_s_reset[["latitude", "longitude"]].values
+        coord_list = df_s_reset[["lat", "lon"]].values
 
     else:
         indices = np.random.randint(len(df_s_reset), size=N)
 
         for i in indices:
             df_location = df_s_reset.iloc[i]
-            lat = df_location["latitude"]
-            lon = df_location["longitude"]
+            lat = df_location["lat"]
+            lon = df_location["lon"]
             coord_list.append([lat, lon])
 
     return coord_list
