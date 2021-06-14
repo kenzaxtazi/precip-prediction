903dfce2db82599a6b62a56ea1bcac90a589ee53
Kenza Tazi <kt2015@ic.ac.uk>
2021-06-14 16:37:06 +0100

    functions to load + preprocess APHRODITE data

--- load/aphrodite.py
+++ load/aphrodite.py
@@ -2,10 +2,44 @@
 The APHRODITE dataset is available on in the BAS workspace on JASMIN
 """
 
-def collect_APHRO():
+import LocationSel as ls
+import glob
+import xarray as xr
+import numpy as np
+from tqdm import tqdm
+
+
+def collect_APHRO(location):
     """ Downloads data from APHRODITE model"""
-    aphro_ds = xr.merge([xr.open_dataset(f) for f in glob.glob('/Users/kenzatazi/Downloads/APHRO_MA_025deg_V1101.1951-2007.gz/*')])
-    aphro_ds = aphro_ds.assign_attrs(plot_legend="APHRODITE") # in mm/day   
-    aphro_ds = aphro_ds.rename_vars({'precip': 'tp'})
-    aphro_ds['time'] = standardised_time(aphro_ds)
-    return aphro_ds
+    aphro_ds = xr.open_dataset("Data/APHRODITE/aphrodite_indus_1951_2016.nc")
+    masked_ds = ls.select_basin(aphro_ds, location, interpolate=True)
+    ds = masked_ds.assign_attrs(plot_legend="APHRODITE") # in mm/day   
+    return ds
+
+
+def merge_og_files():
+    """ Function to open, crop and merge the APHRODITE data """
+
+    ds_list = []
+    extent = ls.basin_extent('indus')
+
+    print('1951-2007')
+    for f in tqdm(glob.glob('Data/APHRODITE/APHRO_MA_025deg_V1101.1951-2007.gz/*.nc')):
+        ds = xr.open_dataset(f)
+        ds = ds.rename({'latitude': 'lat', 'longitude': 'lon', 'precip': 'tp'})
+        ds_cropped = ds.tp.sel(lon=slice(extent[1], extent[3]), lat=slice(extent[2], extent[0]))
+        ds_resampled = (ds_cropped.resample(time="M")).mean()
+        ds_resampled['time'] = ds_resampled.time.astype(float)/365/24/60/60/1e9 +1970
+        ds_list.append(ds_resampled)
+
+    print('2007-2016')
+    for f in tqdm(glob.glob('Data/APHRODITE/APHRO_MA_025deg_V1101_EXR1/*.nc')):
+        ds = xr.open_dataset(f)
+        ds = ds.rename({'precip': 'tp'})
+        ds_cropped = ds.tp.sel(lon=slice(extent[1], extent[3]), lat=slice(extent[2], extent[0]))
+        ds_resampled = (ds_cropped.resample(time="M")).mean()
+        ds_resampled['time'] = ds_resampled.time.astype(float)/365/24/60/60/1e9 +1970
+        ds_list.append(ds_resampled)
+    
+    ds_merged = xr.merge(ds_list)
+    ds_merged.to_netcdf("Data/APHRODITE/aphrodite_indus_1951_2016.nc")
