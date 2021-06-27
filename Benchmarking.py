39108acedf9e776f735ac95ce1e705031d574094
Kenza Tazi <kt2015@ic.ac.uk>
2021-06-27 21:47:36 +0100

    rm obsolete locat° sel f° +dataset_stats() metrics

--- Benchmarking.py
+++ Benchmarking.py
@@ -20,21 +20,11 @@ import Timeseries as tims
 import PDF as pdf
 import Maps
 
+from sklearn.metrics import mean_squared_error, r2_score
+
+
 model_filepath = 'Models/model_2021-01-01/08/21-22-35-56'
 
-
-def select_coords(dataset, lat=None, lon=None):
-    """ Interpolate dataset at given coordinates """
-    timeseries = dataset.interp(coords={"lon": lon, "lat": lat}, method="nearest")
-    timeseries = timeseries.sel(time= slice(1990, 2005))
-    return timeseries
-
-def select_basin(dataset, location):
-    """ Interpolate dataset at given coordinates """  
-    mask_filepath = dp.find_mask(location)
-    basin = dd.apply_mask(dataset, mask_filepath) 
-    basin = basin.sel(time= slice(1990, 2005))  
-    return basin
 
 def model_prep(location, data_filepath='Data/model_pred_test.csv', model_filepath='Models/model_2021-01-01/08/21-22-35-56', minyear=1990, maxyear=2005, ):
     """ Prepares model outputs for comparison """
@@ -69,7 +59,7 @@ def model_prep(location, data_filepath='Data/model_pred_test.csv', model_filepat
     return model_ds
 
 
-def dataset_stats(datasets):
+def dataset_stats(datasets, ref_ds=None):
     """ Print mean, standard deviations and slope for datasets """
 
     for ds in datasets:
@@ -78,34 +68,45 @@ def dataset_stats(datasets):
         tp = ds.tp.values
         if len(tp.shape)>1:
             ds = dp.average_over_coords(ds)
-        
-        slope, intercept, r_value, p_value, std_err = stats.linregress(ds.time.values, ds.tp.values)
+        da = ds['tp'].dropna(dim='time')
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(da.time.values, da.values)
         print(name)
-        print('mean = ', np.mean(ds.tp.values),'mm/day')
-        print('std = ', np.std(ds.tp.values), 'mm/day')
-        print('slope = ', slope, 'mm/day/month')
+        print('mean = ', np.mean(da.values),'mm/day')
+        print('std = ', np.std(da.values), 'mm/day')
+        print('slope = ', slope, 'mm/day/year')
+
+        if ref_ds is not None:
+            tp_ref = ref_ds.tp.values
+            df = pd.DataFrame({'tp_ref': tp_ref, 'tp': tp})
+            df = df.dropna()
+            r2 = r2_score(df['tp_ref'].values, df['tp'].values)
+            rmse = mean_squared_error(df['tp_ref'].values, df['tp'].values, squared=False)
+            print('R2 = ', r2)
+            print('RMSE = ', rmse)
 
 
-def single_location_comparison(location=[31.65,77.34]):
+
+def single_location_comparison(location=[31.77,76.933]):
     """ Plots model outputs for given coordinates over time """
     
     aphro_ds = aphrodite.collect_APHRO(location, minyear=2000, maxyear=2011)
     cru_ds= cru.collect_CRU(location, minyear=2000, maxyear=2011)
     era5_ds = era5.collect_ERA5(location, minyear=2000, maxyear=2011)
-    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2011)
+    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2010.99)
     wrf_ds = beas_sutlej_wrf.collect_BC_WRF(location, minyear=2000, maxyear=2011)
-    gauge_ds = beas_sutlej_gauges.gauge_download('Banjar')
+    gauge_ds = beas_sutlej_gauges.gauge_download('Banjar', minyear=2000, maxyear=2011)
     
     # cmip_ds = dd.collect_CMIP5()
     # cordex_ds = dd.collect_CORDEX()
     # model_ts = model_prep([lat, lon], data_filepath='single_loc_test.csv', model_filepath=model_filepath)
     
-    timeseries = [aphro_ds, cru_ds,  era5_ds, gpm_ds, wrf_ds, gauge_ds] #, cmip_ts, cru_ts, aphro_ts]
+    timeseries = [era5_ds, gpm_ds, gauge_ds] #aphro_ds, cru_ds, wrf_ds, ]
 
     tims.benchmarking_plot(timeseries)
-    dataset_stats(timeseries)
-    corr.dataset_correlation(timeseries)
-    pdf.benchmarking_plot(timeseries)
+    dataset_stats(timeseries, ref_ds=gauge_ds)
+    # corr.dataset_correlation(timeseries)
+    pdf.benchmarking_plot(timeseries, kernel_density=False)
 
 
 def basin_comparison(model_filepath, location):
@@ -123,7 +124,7 @@ def basin_comparison(model_filepath, location):
     # cordex_bs = select_basin(cordex_ds, location)
     # model_bs = model_prep(location, model_filepath)
 
-    basins = [aphro_ds, cru_ds,  era5_ds, gpm_ds, wrf_ds]
+    basins = [aphro_ds, cru_ds, era5_ds, gpm_ds, wrf_ds]
 
     dataset_stats(basins)
 
