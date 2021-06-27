93f2dda536134ee84daaa43e8a14ba02608fdeeb
Kenza Tazi <kt2015@ic.ac.uk>
2021-06-27 21:54:53 +0100

    updated information for dataset loading

--- load/__init__.py
+++ load/__init__.py
@@ -3,21 +3,11 @@ Datasets should aim to all have similar formats so they can be easily used and c
 
 - lon for longitude in °E (float)
 - lat for latitude in °N (float)
-- time for time in years (float)
+- time for time in years (float) with monthly mean taken in the middle of each month (ERA5 formatting) 
 - tp for total precipitation in mm/day/month (float)
 
 Datasets should be returned as xarray Datasets and saved as netcdf files when possible.
 """
 
 
-# Dataset class
-
-'''
-class Dataset(dataset, timerange=None, location=None):
-
-    def __init__(self):
-        
-        # load dataset from name given
-        # mask or interp to location
-        # slice to time range
-'''
+# Dataset class?
