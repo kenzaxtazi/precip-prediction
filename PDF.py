132322e9e5144164ff5c1723a25555b3134729af
Kenza Tazi <kt2015@ic.ac.uk>
2021-07-01 14:23:54 +0100

    mult_gauge_loc_plot() + cdf_benchmarking_plot()

--- PDF.py
+++ PDF.py
@@ -1,4 +1,5 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 import xarray as xr
 import cartopy.crs as ccrs
@@ -8,15 +9,20 @@ import matplotlib.ticker as tck
 import matplotlib.cm as cm
 import DataExploration as de
 
+import matplotlib
+
+from sklearn.neighbors import KernelDensity
+from sklearn.model_selection import GridSearchCV
+
 import FileDownloader as fd
 import DataPreparation as dp
 import DataDownloader as dd
 
 
-# Open data
 
-data_filepath = fd.update_cds_monthly_data()
-mask_filepath = "Data/ERA5_Upper_Indus_mask.nc"
+# Open data
+# data_filepath = fd.update_cds_monthly_data()
+# mask_filepath = "Data/ERA5_Upper_Indus_mask.nc"
 
 
 month_labels = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
@@ -24,7 +30,15 @@ month_dict = {0: "January", 1: "February", 2: "March", 3: "April", 4: "May", 5: 
               6: "July", 7: "August", 8: "September", 9: "October", 10: "November", 11: "December"} 
 
 
-def monthly_PDF(data_filepath, mask_filepath, variable="tp", longname=""):
+def monthly_PDF(timeseries, variable="tp", longname=""):
+
+    combined_df = pd.DataFrame()
+    
+    for ts in timeseries:
+        df1 = ts.tp.to_dataframe(name=ts.plot_legend)
+        df2 = df1.dropna().reset_index()
+        df3 = df2.drop(["time", "lon", "lat"], axis=1)
+        combined_df[ts.plot_legend]= df3[ts.plot_legend]
 
     df = dd.download_data(mask_filepath)
     clean_df = df.dropna()
@@ -91,7 +105,72 @@ def monthly_PDF(data_filepath, mask_filepath, variable="tp", longname=""):
     plt.show()
 
 
-def benchmarking_plot(timeseries):
+def benchmarking_plot(timeseries, kernel_density=False):
+    """ Plot probability distribution of model outputs """
+
+    combined_df = pd.DataFrame()
+    
+    for ts in timeseries:
+        df1 = ts.tp.to_dataframe(name=ts.plot_legend)
+        df2 = df1.reset_index()
+        df3 = df2.drop(["time", "lon", "lat"], axis=1)
+        print(df3.shape)
+        combined_df[ts.plot_legend]= df3[ts.plot_legend]
+    
+    time_ds = timeseries[0].time.to_dataframe(name='time')
+    months_float = np.ceil((time_ds["time"] - np.floor(time_ds["time"])) * 12)
+    df =  combined_df.copy()
+    df["time"] = months_float.values
+    df["time"] = df["time"].astype(int)
+
+    grouped_dfs = []
+
+    for m in range(1, 13):
+        month_df = df[df['time'] == m]
+        month_df = month_df.drop(['time'], axis=1)
+        grouped_dfs.append(month_df)
+    
+    # Plot 
+    fig, axs = plt.subplots(3, 4, sharex=True, sharey=True)
+    
+    for i in range(12):
+        x = (i) % 3
+        y = int(i / 3)
+        for b in list(combined_df):
+            data = grouped_dfs[i][b].values
+            X_plot = np.linspace(0, data.max(), 1000)[:, np.newaxis]
+            if kernel_density == True:
+                bandwidth = np.arange(0.05, 2, .05)
+                X = data.reshape(-1, 1)
+                kde = KernelDensity(kernel='gaussian')
+                grid = GridSearchCV(kde, {'bandwidth': bandwidth})
+                grid.fit(X)
+                kde = grid.best_estimator_
+                log_dens = kde.score_samples(X_plot)
+                axs[x, y].fill(X_plot[:, 0], np.exp(log_dens), label=b)
+            else:
+                axs[x, y].hist((grouped_dfs[i])[b], density=True, label=b, bins=np.arange(0, int(combined_df.max().max()))) #alpha=.5)
+        axs[x, y].set_title(month_dict[i])
+        axs[x, y].set_title(month_dict[i])
+        axs[x, y].xaxis.set_tick_params(which="both", labelbottom=True)
+        axs[x, y].yaxis.set_tick_params(which="both", labelbottom=True)
+        axs[x, y].set_xlabel("Precipitation (mm/day)")
+        axs[x, y].set_ylabel("Probability density")
+        
+        '''
+        axs[x, y].axvline(
+            np.percentile((grouped_dfs[i])['ERA5'], 95),
+            color="k",
+            linestyle="dashed",
+            linewidth=1,
+            label="ERA5 95th percentile",
+        )
+        '''
+    plt.legend(loc="upper right")    
+    plt.show()
+
+
+def cdf_benchmarking_plot(timeseries, kernel_density=False):
     """ Plot probability distribution of model outputs """
 
     combined_df = pd.DataFrame()
@@ -114,20 +193,38 @@ def benchmarking_plot(timeseries):
         month_df = df[df['time'] == m]
         month_df = month_df.drop(['time'], axis=1)
         grouped_dfs.append(month_df)
-
+    
+    # Plot 
     fig, axs = plt.subplots(3, 4, sharex=True, sharey=True)
+    
+    
     for i in range(12):
         x = (i) % 3
         y = int(i / 3)
         for b in list(combined_df):
-            axs[x, y].hist((grouped_dfs[i])[b], density=True, label=b) #, bins=np.arange(20),alpha=.5)
+            
+            data = grouped_dfs[i][b].values
+            X_plot = np.linspace(0, data.max(), 1000)[:, np.newaxis]
+            data_sorted = np.sort(data)
+            p = 1. * np.arange(len(data)) / (len(data) - 1)
+
+            if kernel_density == True:
+                n, bins, patches = np.hist((grouped_dfs[i])[b], cumulative=True, density=True, label=b)
+                X = n.reshape(-1, 1)
+                kde = KernelDensity(kernel='gaussian', bandwidth=0.1).fit(X)
+                log_dens = kde.score_samples(X_plot)
+                axs[x, y].plot(X_plot[:, 0], np.exp(log_dens), label=b)
+                
+            else:
+                axs[x, y].hist((grouped_dfs[i])[b], cumulative=True, density=True, label=b)
+
         axs[x, y].set_title(month_dict[i])
         axs[x, y].set_title(month_dict[i])
         axs[x, y].xaxis.set_tick_params(which="both", labelbottom=True)
         axs[x, y].yaxis.set_tick_params(which="both", labelbottom=True)
         axs[x, y].set_xlabel("Precipitation (mm/day)")
-        axs[x, y].set_ylabel("Probability density")
-        '''
+        axs[x, y].set_ylabel("CDF")
+        
         axs[x, y].axvline(
             np.percentile((grouped_dfs[i])['ERA5'], 95),
             color="k",
@@ -135,6 +232,55 @@ def benchmarking_plot(timeseries):
             linewidth=1,
             label="ERA5 95th percentile",
         )
-        '''
+        
     plt.legend(loc="upper right")    
     plt.show()
+
+
+def mult_gauge_loc_plot(gauge_ds, other_datasets):
+    """ Plot probability distribution of datasets for mulitple gauge locations """
+
+    gauge_df = gauge_ds.to_dataframe()
+    ind_df = gauge_df.reset_index()
+    melt_df = pd.melt(ind_df, id_vars=['time'], value_name='Gauge data')
+
+    combined_df = melt_df[['time', 'Gauge data']]
+
+    for ds in other_datasets:
+        df1 = ds.to_dataframe() 
+        combined_df[ds.plot_legend] = df1.tp.values
+
+    combined_df["time"] = np.ceil((combined_df["time"] - np.floor(combined_df["time"])) * 12)
+    combined_df["time"] = combined_df["time"].astype(int)
+        
+
+    month_list = []
+
+    for m in range(1, 13):
+        month_df = combined_df[combined_df['time'] == m]
+        month_df = month_df.drop(['time'], axis=1)
+        month_list.append(month_df)
+        
+    # Plot 
+    fig, axs = plt.subplots(3, 4)#, sharex=True, sharey=True)
+    
+    for i in range(12):
+        x = (i) % 3
+        y = int(i / 3)
+
+        if i == 11:
+            leg = True
+        else:
+            leg =False
+
+        sns.kdeplot(data=month_list[i].drop(['Gauge data'], axis=1), linestyle='--', ax=axs[x, y], bw_adjust=1, legend=leg, common_norm=False)
+        sns.kdeplot(data=month_list[i]['Gauge data'], c='k', ax=axs[x, y], bw_adjust=1, legend=leg)
+
+        axs[x, y].set_title(month_dict[i])
+        axs[x, y].xaxis.set_tick_params(which="both", labelbottom=True)
+        axs[x, y].yaxis.set_tick_params(which="both", labelbottom=True)
+        axs[x, y].set_xlabel("Precipitation (mm/day)")
+        axs[x, y].set_ylabel("Probability density")
+
+    plt.show()
+
