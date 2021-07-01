f6a2b7eb78398fef5ae89128789a85a7144a0fcf
Kenza Tazi <kt2015@ic.ac.uk>
2021-07-01 14:23:11 +0100

    multi_location_comparison()

--- Benchmarking.py
+++ Benchmarking.py
@@ -93,7 +93,7 @@ def single_location_comparison(location=[31.77,76.933]):
     aphro_ds = aphrodite.collect_APHRO(location, minyear=2000, maxyear=2011)
     cru_ds= cru.collect_CRU(location, minyear=2000, maxyear=2011)
     era5_ds = era5.collect_ERA5(location, minyear=2000, maxyear=2011)
-    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2010.99)
+    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2011)
     wrf_ds = beas_sutlej_wrf.collect_BC_WRF(location, minyear=2000, maxyear=2011)
     gauge_ds = beas_sutlej_gauges.gauge_download('Banjar', minyear=2000, maxyear=2011)
     
@@ -101,9 +101,9 @@ def single_location_comparison(location=[31.77,76.933]):
     # cordex_ds = dd.collect_CORDEX()
     # model_ts = model_prep([lat, lon], data_filepath='single_loc_test.csv', model_filepath=model_filepath)
     
-    timeseries = [era5_ds, gpm_ds, gauge_ds] #aphro_ds, cru_ds, wrf_ds, ]
+    timeseries = [gauge_ds, era5_ds, gpm_ds, aphro_ds, cru_ds, wrf_ds, ]
 
-    tims.benchmarking_plot(timeseries)
+    tims.benchmarking_subplots(timeseries, reference_dataset=gauge_ds)
     dataset_stats(timeseries, ref_ds=gauge_ds)
     # corr.dataset_correlation(timeseries)
     pdf.benchmarking_plot(timeseries, kernel_density=False)
@@ -115,7 +115,7 @@ def basin_comparison(model_filepath, location):
     aphro_ds = aphrodite.collect_APHRO(location, minyear=2000, maxyear=2011)
     cru_ds= cru.collect_CRU(location, minyear=2000, maxyear=2011)
     era5_ds = era5.collect_ERA5(location, minyear=2000, maxyear=2011)
-    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2011)
+    gpm_ds = gpm.collect_GPM(location,  minyear=2000, maxyear=2010.99)
     wrf_ds = beas_sutlej_wrf.collect_BC_WRF(location, minyear=2000, maxyear=2011)
     
     # cmip_ds = dd.collect_CMIP5()
@@ -127,6 +127,55 @@ def basin_comparison(model_filepath, location):
     basins = [aphro_ds, cru_ds, era5_ds, gpm_ds, wrf_ds]
 
     dataset_stats(basins)
+
+
+def multi_location_comparison():
+    """ Plots model outputs for given coordinates over time """
+
+    gauge_ds = beas_sutlej_gauges.all_gauge_data(minyear=2000, maxyear=2011)
+    locations = [[31.65, 77.34], [31.424, 76.417], [31.80, 77.19], [31.357, 76.878], [31.57, 77.22], 
+                 [31.67,77.06], [31.454,77.644], [31.77, 77.31], [31.238,77.108], [31.88, 77.15]]
+
+    aphro_sets = []
+    cru_sets = []
+    era5_sets = []
+    gpm_sets = []
+    wrf_sets = []
+
+    for l in locations:
+        aphro_ds = aphrodite.collect_APHRO(l, minyear=2000, maxyear=2011)
+        aphro_sets.append(aphro_ds.tp)
+        
+        cru_ds= cru.collect_CRU(l, minyear=2000, maxyear=2011)
+        cru_sets.append(cru_ds.tp)
+        
+        era5_ds = era5.collect_ERA5(l, minyear=2000, maxyear=2011)
+        era5_sets.append(era5_ds.tp)
+        
+        gpm_ds = gpm.collect_GPM(l,  minyear=2000, maxyear=2011)
+        gpm_sets.append(gpm_ds.tp)
+
+        wrf_ds = beas_sutlej_wrf.collect_BC_WRF(l, minyear=2000, maxyear=2011)
+        wrf_sets.append(wrf_ds.tp)
+
+    # Merge datasets
+    aphro_mer_ds = xr.concat(aphro_sets, dim='lon')
+    aphro_mer_ds.attrs['plot_legend'] = 'APHRODITE'
+
+    cru_mer_ds = xr.concat(cru_sets, dim='lon')
+    cru_mer_ds.attrs['plot_legend'] = 'CRU'
+
+    era5_mer_ds = xr.concat(era5_sets, dim='lon')
+    era5_mer_ds.attrs['plot_legend'] = 'ERA5'
+
+    gpm_mer_ds = xr.concat(gpm_sets, dim='lon')
+    gpm_mer_ds.attrs['plot_legend'] = 'GPM'
+
+    wrf_mer_ds = xr.concat(wrf_sets, dim='lon')
+    wrf_mer_ds.attrs['plot_legend'] = 'WRF_BC'
+     
+    timeseries = [gauge_ds, era5_mer_ds, gpm_mer_ds, aphro_mer_ds, cru_mer_ds, wrf_mer_ds]
+    pdf.mult_gauge_loc_plot((timeseries)
 
 
 
