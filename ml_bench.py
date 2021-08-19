606618f56a65544a866df54c61c587a08ce58bf6
Kenza Tazi <kt2015@ic.ac.uk>
2021-08-19 11:17:10 +0100

    loat lon elv only

--- ml_bench.py
+++ ml_bench.py
@@ -106,16 +106,16 @@ lf_train['tp_tr'] = dp.log_transform(lf_train['tp'].values)
 val_df['tp_tr'] = dp.log_transform(val_df['tp'].values)
 
 # To arrays
-hf_x_train = hf_train[['time', 'lon', 'lat', 'z', 'slope']].values.reshape(-1,5)
+hf_x_train = hf_train[['time', 'lon', 'lat', 'z',].values.reshape(-1,4)
 hf_y_train = hf_train['tp'].values.reshape(-1,1)
 
 lf_y_train_log = lf_train.tp_tr.values.reshape(-1, 1)
 hf_y_train_log = hf_train.tp_tr.values.reshape(-1,1)
 
-lf_x_train = lf_train[['time', 'lon', 'lat', 'z', 'slope']].values.reshape(-1,5)
+lf_x_train = lf_train[['time', 'lon', 'lat', 'z'].values.reshape(-1,4)
 lf_y_train = lf_train['tp'].values.reshape(-1,1)
 
-x_val = val_df[['time', 'lon', 'lat', 'z', 'slope']].values.reshape(-1,5)
+x_val = val_df[['time', 'lon', 'lat', 'z']].values.reshape(-1,4)
 y_val = val_df['tp'].values.reshape(-1,1)
 
 X_train, Y_train = convert_xy_lists_to_arrays([lf_x_train, hf_x_train], [lf_y_train, hf_y_train])
@@ -126,7 +126,7 @@ X_train, Y_train_log = convert_xy_lists_to_arrays([lf_x_train, hf_x_train], [lf_
 
 # Linear MFDGP 
 def linear_mfdgp(X_train, Y_train):
-    kernels = [GPy.kern.RBF(5), GPy.kern.RBF(5)]
+    kernels = [GPy.kern.RBF(4), GPy.kern.RBF(4)]
     lin_mf_kernel = emukit.multi_fidelity.kernels.LinearMultiFidelityKernel(kernels)
     gpy_lin_mf_model = GPyLinearMultiFidelityModel(X_train, Y_train, lin_mf_kernel, n_fidelities=2)
     gpy_lin_mf_model.mixed_noise.Gaussian_noise.fix(0)
@@ -137,7 +137,7 @@ def linear_mfdgp(X_train, Y_train):
 
 # Linear MFDGP with log transform
 def log_linear_mfdgp(X_train, Y_train):
-    kernels = [GPy.kern.RBF(5), GPy.kern.RBF(5)]
+    kernels = [GPy.kern.RBF(4), GPy.kern.RBF(4)]
     lin_mf_kernel = emukit.multi_fidelity.kernels.LinearMultiFidelityKernel(kernels)
     gpy_lin_mf_model = GPyLinearMultiFidelityModel(X_train, Y_train, lin_mf_kernel, n_fidelities=2)
     gpy_lin_mf_model.mixed_noise.Gaussian_noise.fix(0)
@@ -170,7 +170,7 @@ def log_nonlinear_mfdgp(X_train, Y_train):
 
 # Simple GP with log transform
 def log_gp(x_train_h, y_train_h):
-    kernel = GPy.kern.StdPeriodic(1, period=1) * GPy.kern.RBF(1) + GPy.kern.RBF(5)
+    kernel = GPy.kern.StdPeriodic(1, period=1) * GPy.kern.RBF(1) + GPy.kern.RBF(4)
     m = GPy.models.GPRegression(x_train_h, y_train_h, kernel)
     m.Gaussian_noise.fix(0)
     m.optimize_restarts(num_restarts=5)
@@ -178,7 +178,7 @@ def log_gp(x_train_h, y_train_h):
 
 # Simple GP
 def gp(x_train_h, y_train_h):
-    kernel = GPy.kern.StdPeriodic(1, period=1) * GPy.kern.RBF(1) + GPy.kern.RBF(5)
+    kernel = GPy.kern.StdPeriodic(1, period=1) * GPy.kern.RBF(1) + GPy.kern.RBF(4)
     m = GPy.models.GPRegression(x_train_h, y_train_h, kernel)
     m.Gaussian_noise.fix(0)
     m.optimize_restarts(num_restarts=5)
@@ -226,8 +226,8 @@ nl_mf_h_y_pred, mf_h_y_std_pred = nonlin_mf_model.predict(x_met[n:])
 log_gp_y, log_gp_var = dp.inverse_log_transform(log_gp_m.predict(x_val))
 gp_y, gp_var = gp_m.predict(x_val)
 
-log_lin_y = dp.inverse_log_transform(log_linear_m.predict(x_val.reshape(-1,5)))
-lin_y = linear_m.predict(x_val.reshape(-1,5))
+log_lin_y = dp.inverse_log_transform(log_linear_m.predict(x_val.reshape(-1,4)))
+lin_y = linear_m.predict(x_val.reshape(-1,4))
 
 
 ## R2
@@ -249,7 +249,6 @@ log_lin_r2 = r2_score(y_val, log_lin_y)
 
 
 ## RMSE
-
 lin_mf_h_rmse = mean_squared_error(y_val, lin_mf_h_y_pred, squared=False)
 lin_mf_l_rmse = mean_squared_error(y_val, lin_mf_l_y_pred, squared=False)
 
