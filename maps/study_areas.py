57d75134702eaa90b613f893a55c1117ca76ce1d
Kenza Tazi <kt2015@ic.ac.uk>
2021-10-22 15:51:46 +0100

    formatting

--- maps/study_areas.py
+++ maps/study_areas.py
@@ -59,10 +59,11 @@ def global_map(uib_only=False, bs_only=False):
 
 
 def indus_map():
-    """Return two maps: global map with box, zoomed in map of the Upper Indus Basin."""
+    """Return two maps: global map, zoomed in map of the Upper Indus Basin."""
 
     # River shapefiles
-    fpath = "_Data/Shapefiles/ne_50m_rivers_lake_centerlines_scale_rank/ne_50m_rivers_lake_centerlines_scale_rank.shp"
+    fpath = "_Data/Shapefiles/ne_50m_rivers_lake_centerlines_scale_rank/\
+        ne_50m_rivers_lake_centerlines_scale_rank.shp"
     as_shp = shapereader.Reader(fpath)
 
     # UIB shapefile
@@ -70,7 +71,8 @@ def indus_map():
     uib_shape = shapereader.Reader(uib_path)
 
     # Beas shapefile and projection
-    beas_path = "_Data/Shapefiles/beas-sutlej-shapefile/beas_sutlej_basins/beas_watershed.shp"
+    beas_path = "_Data/Shapefiles/beas-sutlej-shapefile/beas_sutlej_basins/\
+        beas_watershed.shp"
     beas_shape = shapereader.Reader(beas_path)
     beas_globe = ccrs.Globe(semimajor_axis=6377276.345,
                             inverse_flattening=300.8017)
@@ -79,7 +81,8 @@ def indus_map():
         false_northing=0.0, standard_parallels=[30, 30], globe=beas_globe)
 
     # Sutlej shapefile and projection
-    stlj_path = "_Data/Shapefiles/beas-sutlej-shapefile/beas_sutlej_basins/sutlej_watershed.shp"
+    stlj_path = "_Data/Shapefiles/beas-sutlej-shapefile/beas_sutlej_basins/\
+        sutlej_watershed.shp"
     stlj_shape = shapereader.Reader(stlj_path)
     stlj_globe = ccrs.Globe(semimajor_axis=6377276.345,
                             inverse_flattening=300.8017)
@@ -123,9 +126,8 @@ def indus_map():
     for rec in uib_shape.records():
         ax.add_geometries(
             [rec.geometry],
-            ccrs.AlbersEqualArea(
-                central_longitude=125, central_latitude=-15, standard_parallels=(7, -32)
-            ),
+            ccrs.AlbersEqualArea(central_longitude=125, central_latitude=-15,
+                                 standard_parallels=(7, -32)),
             edgecolor="red",
             facecolor="red",
             alpha=0.1,
@@ -220,15 +222,18 @@ def beas_sutlej_gauge_map():
     filepath = '_Data/stations_MGM.xlsx'
     df = pd.read_excel(filepath)
 
+    '''
     # Beas and Sutlej shapefile and projection
     bs_path = "_Data/Shapefiles/beas-sutlej-shapefile/12500Ha.shp"
     bs_shape = shapereader.Reader(bs_path)
     bs_globe = ccrs.Globe(semimajor_axis=6377276.345,
-                          inverse_flattening=300.8017)
+                          inverse_flattening = 300.8017)
     cranfield_crs = ccrs.LambertConformal(
-        central_longitude=82, central_latitude=20, false_easting=2000000.0, false_northing=2000000.0,
-        standard_parallels=[12.47294444444444, 35.17280555555556], globe=bs_globe)
-
+        central_longitude=82, central_latitude=20, false_easting=2000000.0,
+        false_northing=2000000.0,
+        standard_parallels=[12.47294444444444, 35.17280555555556],
+        globe=bs_globe)
+    '''
     # Topography
     top_ds = xr.open_dataset(
         '/Users/kenzatazi/Downloads/GMTED2010_15n015_00625deg.nc')
@@ -243,7 +248,9 @@ def beas_sutlej_gauge_map():
     ax.set_extent([75.5, 78.5, 30.25, 33.5])
     gl = ax.gridlines(draw_labels=True)
     divnorm = colors.TwoSlopeNorm(vmin=-500., vcenter=0, vmax=6000.)
-    top_ds.elevation.plot.contourf(cmap='gist_earth', levels=np.arange(0, 6000, 100), norm=divnorm,
+    top_ds.elevation.plot.contourf(cmap='gist_earth',
+                                   levels=np.arange(0, 6000, 100),
+                                   norm=divnorm,
                                    cbar_kwargs={'label': 'Elevation [m]'})
     # for rec in bs_shape.records():
     #  ax.add_geometries([rec.geometry], cranfield_crs, edgecolor='None',
@@ -262,9 +269,9 @@ def beas_sutlej_gauge_map():
     # df_train = df[df['Longitude (o)'] > 77]
     # ax.scatter(df_train['Longitude (o)'],  df['Latitude (o)'], s=5, c='y', \
     # label='Test locations')
-    # df_validation = df[df['Longitude (o)'] < 77]
-    # ax.scatter(df_validation['Longitude (o)'],  df_validation['Latitude (o)'], \
-    # s=5, c='b', label='Val locations')
+    # df_validation = df[df['Longitude (o)'] < 77] \
+    # ax.scatter(df_validation['Longitude (o)'], \
+    # df_validation['Latitude (o)'], \s=5, c='b', label='Val locations')
     val_set = np.array([[31.65, 77.34], [31.424, 76.417], [31.80, 77.19],
                         [31.357, 76.878], [31.52, 77.22], [31.67, 77.06],
                         [31.454, 77.644], [31.77, 77.31], [31.238, 77.108],
