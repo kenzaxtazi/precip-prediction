2da747c53dd9841499e6bf1e2624c90d44fcf37f
Kenza Tazi <ktazi100@yahoo.co.uk>
2024-10-18 17:13:40 +0100

    updated feature selection

--- experiments/wbm/feature_selection_wbm.py
+++ experiments/wbm/feature_selection_wbm.py
@@ -8,12 +8,12 @@
 
 import gpflow
 import sys
-sys.path.append('/Users/kenzatazi/Documents/CDT/Code/precip-prediction/')
+sys.path.append('/data/hpcdata/users/kenzi22/')
+sys.path.append('/data/hpcdata/users/kenzi22/precip-prediction')
 
 import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
 import tensorflow_probability as tfp
 import tensorflow as tf
 
@@ -23,84 +23,120 @@ import gp.gp_models as gpm
 from scipy.special import inv_boxcox
 
 from load import era5, data_dir
-import pickle
 
+tf.random.set_seed(42)
 
+### Load
 
-# ## Load
-
-
-def new_kernel_list(old_kernel, iter=int):
+def new_kernel_list(old_dim_list, itera=int):
     """ Build new kernels """
 
-    new_kernel = gpflow.kernels.Matern32(lengthscales=1, variance=1, active_dims=[iter+3])
+    N = len(old_dim_list)
+    new_dim_list = old_dim_list + [itera+3]
 
-    k1 = old_kernel + new_kernel
-    #k2 = old_kernel * new_kernel
-    k3 = old_kernel
-
-    kernel_list = [k1, k3] #k2
+    ### Old kernel
+    k1a = gpflow.kernels.Periodic(gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0]), period=1./35.)
+    k1b = gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0])
+    k1c = gpflow.kernels.Matern32(lengthscales=[1e-2,1e-2], active_dims=[1,2])
+    old_kernel = k1a * k1b + k1c
     
-    return kernel_list
+    if not old_dim_list:
+        pass
+    else:
+        for i in np.arange(len(old_dim_list)):
+            k1d = gpflow.kernels.Matern32(lengthscales=1e-2, active_dims=[old_dim_list[i]])
+            old_kernel += k1d
 
-def fit_kernel(xtrain, xval, ytrain_tr, yval_tr, lmbda, kernel: gpflow.kernels.Kernel)-> tuple:
+    ### New kernel
+    k2a = gpflow.kernels.Periodic(gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0]), period=1./35.)
+    k2b = gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0])
+    k2c = gpflow.kernels.Matern32(lengthscales=[1e-2,1e-2], active_dims=[1,2])
+    new_kernel = k2a * k2b + k2c
+            
+    for i in np.arange(len(new_dim_list)):
+        k2d = gpflow.kernels.Matern32(lengthscales=1e-2, active_dims=[new_dim_list[i]])
+        new_kernel += k2d
+    
+    kernel_list = [new_kernel, old_kernel]
+    dim_list = [new_dim_list, old_dim_list]
+    
+    return kernel_list, dim_list
+
+def fit_kernel(xtrain, xval, ytrain_tr, yval_tr, kern: gpflow.kernels.Kernel)-> tuple:
     """ Train model and return BIC and MLL for each location"""
         
     # train model
-    m = gpm.multi_gp(xtrain, xval, ytrain_tr, yval_tr, lmbda_bc=lmbda, kernel=kernel, print_perf=False)
+    m = gpflow.models.GPR(data=(xtrain.reshape(-1, 21), ytrain_tr.reshape(-1, 1)), kernel=kern)
     opt = gpflow.optimizers.Scipy()
-    opt.minimize(m.training_loss, m.trainable_variables)
+    opt.minimize(m.training_loss, m.trainable_variables, options={'maxiter':200})
+    gpflow.utilities.print_summary(m)
 
     # Calculate Bayesian Information Criterion and Mean Log Likelihood
-    bic = me.BIC(m, xval, yval_tr.reshape(-1, 1))
-    return bic
+    #bic = me.BIC(m, xval.reshape(-1, 21), yval_tr.reshape(-1, 1))
+    y_pred,_ = m.predict_y(xval)
+    r2 = me.R2(yval_tr, y_pred.numpy())
+    print(r2)
+    return r2
 
 
-xtrain, xval, xtest, ytrain_tr, yval_tr, ytest_tr, lmbda= dp.areal_model_new(location='uib', length=2000, maxyear='2020')
+dataset = dp.areal_model_new(location='uib', length=5000, maxyear='2020', all_var=True)
+xtrain, xval, xtest, ytrain_tr, yval_tr, ytest_tr = dataset.sets()
+
 
 # Locally periodic kernel wrt to time as we want period to be flexible 
 
-k1 = gpflow.kernels.Periodic(gpflow.kernels.Matern32(lengthscales=1, variance=1, active_dims=[0]))
-k1a = gpflow.kernels.Matern32(lengthscales=1, variance=1, active_dims=[0])
-k2 = gpflow.kernels.Matern32(active_dims=[1,2])
-ka = k1 * k1a + k2
+ka = gpflow.kernels.Periodic(gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0]), period=1/35.)
+kb = gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0])
+kc = gpflow.kernels.Matern32(lengthscales=[1e-2,1e-2], active_dims=[1,2])
+k01 = ka * kb + kc
+
+kd = gpflow.kernels.Periodic(gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0]), period=1/35.)
+ke = gpflow.kernels.Matern32(lengthscales=1e-2, variance=1, active_dims=[0])
+kf = gpflow.kernels.Matern32(lengthscales=[1e-2,1e-2], active_dims=[1,2])
+k02 = kd * ke + kf
 
 # Lists
-BIC_lists = []
-kernel_list = [ka]
+bic_lists = []
+kernel_list = [k01, k02]
+dim_list = [[], []]
 
-for j in tqdm.tqdm(range(16)):
+for j in tqdm.tqdm(range(19)):
     
     model_BIC_list = []
 
-    for k in kernel_list:
-        bic = fit_kernel(xtrain, xval, ytrain_tr, yval_tr, lmbda, k)
-        model_BIC_list.append(bic)
+    for kern1 in kernel_list:
+        #restart_bic = []
+        #for r in range(3):
+        r2 = fit_kernel(xtrain, xval, ytrain_tr.reshape(-1,1), yval_tr.reshape(-1,1), kern1)
+        #restart_bic.append(bic)
+        #avg_bic = np.nanmean(bic)
+        model_BIC_list.append(r2)
 
-    best_index = np.argmin(model_BIC_list)
-    best_kernel = kernel_list[best_index]
-    kernel_list = new_kernel_list(best_kernel, iter=j)
-    BIC_lists.append(model_BIC_list)
+    best_index = np.argmax(model_BIC_list)
+    if j > 0:
+        best_dims = dim_list[best_index]
+    else:
+        best_dims = []
+    kernel_list, dim_list = new_kernel_list(best_dims, itera=j)
+    bic_lists.append(model_BIC_list)
 
-with open("wbm_bic_list", "wb") as fp:   #Pickling
-    pickle.dump(BIC_lists, fp)
+    bic_arr = np.array(bic_lists)
+    np.save('experiments/wbm/wbm_r2.npy', bic_arr)
 
+x_arr = np.arange(19)
 
-BIC_arr = np.array(BIC_lists[1:])
-x = np.arange(1, 16)
+var_list = ["time, lon, lat", "tcwv", "slor", "d2m", "z", 
+            "EOF200U1", "t2m", "EOF850U1", "EOF500U1", "EOF500B2", 
+            "EOF200B1", "anor", "NAO", "EOF500U2", "N34", 
+            "EOF850U2", "EOF500B1", "EOF500C1" , "EOF500C2"]
 
-
-var_list = ["time, lat, lon", "tcwv", "slor", "d2m", "z", "EOF200U",  "t2m", 
-            "EOF850U",  "EOF500U", "EOF500B2", "EOF200B", "anor", "NAO", 
-            "EOF500U2", "N34", "EOF850U2", "EOF200B"]
-
-plt.figure(figsize=(7,5))
-plt.scatter(0, BIC_lists[0], marker='o', c='k')
-plt.scatter(x, BIC_arr[:,0], marker='o', label='additive kernel')
-plt.scatter(x, BIC_arr[:,1], marker='<', label='best previous kernel')
+plt.figure(figsize=(15,5))
+plt.scatter(x_arr[0], bic_arr[0,0], marker='o', c='k')
+plt.scatter(x_arr, bic_arr[:,0], marker='o', label='New kernel')
+plt.scatter(x_arr, bic_arr[:,1], marker='<', label='Best previous kernel')
 plt.ylabel('Bayesian Information Criterion')
-plt.xlabel('Kernel design iterations')
+plt.xlabel('Input features')
+plt.xticks(x_arr, labels=var_list, rotation=45)
 plt.legend()
-plt.xticks(np.arange(0, 16, 1), labels=var_list)
-plt.savefig('wbm_bic.png')
+plt.savefig('experiments/wbm/wbm_bic.png')
 
