82987344611e3cc0d51ca73c901d92d5943b5891
Kenza Tazi <ktazi100@yahoo.co.uk>
2024-10-24 14:04:45 +0100

    updated data prep

--- gp/data_prep.py
+++ gp/data_prep.py
@@ -5,14 +5,252 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 # custom libraries
-#sys.path.append('/Users/kenzatazi/Documents/CDT/Code/')  # noqa
-sys.path.append('/data/hpcdata/users/kenzi22/')  # noqa
+dir_path = '/Users/kenzatazi/Documents/CDT/Code/' # noqa
+# dir_path = '/data/hpcdata/users/kenzi22/' # noqa
+sys.path.append(dir_path)  # noqa
+
 from load import era5, location_sel
 import gp.sampling as sa
 
+
+class point_model():
+
+    def __init__(self, location: str | np.ndarray, seed=42, all_data=True, all_var=False):
+        """
+        Output training, validation and test sets for total precipitation.
+
+        Args:
+            location (str | np.ndarray): 'uib', 'khyber', 'ngari', 'gilgit' or [lat, lon] coordinates.
+            number (int, optional): ensemble run number. Defaults to None.
+            EDA_average (bool, optional): use ERA5 low-res ensemble average. Defaults to False.
+            maxyear (str, optional): end year (inclusive). Defaults to None.
+            seed (int, optional): sampling random generator seed. Defaults to 42.
+            all_var (bool, optional): all the variables studied if True or only final selection for paper if False. Defaults to False.
+
+        Returns:
+            tuple: contains
+                x_train (np.array): training feature vector
+                x_val (np.array): validation feature vector
+                x_test (np.array): testing feature vector
+                y_train (np.array): training output vector
+                y_val (np.array): validation output vector
+                y_test (np.array): testing output vector
+                lmbda: lambda value for Box Cox transformation
+        """
+
+        # Download data 
+        if all_data is True:
+            df_train_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_train_all.csv')
+        if all_data is False:
+            df_train_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_train_7000_'+ str(seed)+'.csv')
+        
+        df_val_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_val_1000_'+ str(seed)+'.csv')
+        df_test_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_test_2000_'+ str(seed)+'.csv')
+
+        # Find location
+        df_train_loc = df_train_all[(df_train_all['lon'] == location[0]) & (df_train_all['lat'] == location[1])]
+        df_val_loc = df_val_all[(df_val_all['lon'] == location[0]) & (df_val_all['lat'] == location[1])]
+        df_test_loc = df_test_all[(df_test_all['lon'] == location[0]) & (df_test_all['lat'] == location[1])] 
+
+        # Variable choice
+        if all_var is True:
+            var_list = ["time", "tcwv", "d2m", "EOF200U",  "t2m", "EOF850U",  "EOF500U", "EOF500B2", "EOF200B",
+                "NAO", "EOF500U2", "N34", "EOF850U2", "EOF500B", "tp",]
+        else:
+            var_list =["time", "tcwv", "EOF200U", "EOF500U", "tp"] #"d2m", "t2m", "tp",]
+
+        df_train = df_train_loc[var_list]
+        df_val = df_val_loc[var_list]
+        df_test = df_test_loc[var_list]
+
+        # Standardize time
+        df_train["time"] = pd.to_datetime(df_train["time"])
+        df_train["time"] = pd.to_numeric(df_train["time"])
+        df_val["time"] = pd.to_datetime(df_val["time"])
+        df_val["time"] = pd.to_numeric(df_val["time"])
+        df_test["time"] = pd.to_datetime(df_test["time"])
+        df_test["time"] = pd.to_numeric(df_test["time"])
+
+        df_train['tp'].loc[df_train['tp'] <= 0.0] = 0.0001
+        df_val['tp'].loc[df_val['tp'] <= 0.0] = 0.0001
+        df_test['tp'].loc[df_test['tp'] <= 0.0] = 0.0001
+        
+        # Keep first of 70% for training
+        xtrain = df_train.drop(columns=["tp"]).values
+        ytrain = df_train['tp'].values
+        xval = df_val.drop(columns=["tp"]).values
+        yval = df_val['tp'].values
+        xtest = df_test.drop(columns=["tp"]).values
+        ytest = df_test['tp'].values
+
+        # Precipitation transformation
+        ytrain_tr, lmbda = sp.stats.boxcox(ytrain)
+        yval_tr = sp.stats.boxcox(yval, lmbda=lmbda)
+        ytest_tr = sp.stats.boxcox(ytest, lmbda=lmbda)
+   
+        # Features scaling
+        xscaler = MinMaxScaler()
+        xtrain = xscaler.fit_transform(xtrain.astype(np.float64))
+        xval = xscaler.transform(xval)
+        xtest = xscaler.transform(xtest)
+
+        yscaler = StandardScaler()
+        ytrain_sc = yscaler.fit_transform(ytrain_tr.reshape(-1,1))
+        yval_sc = yscaler.transform(yval_tr.reshape(-1,1))
+        ytest_sc = yscaler.transform(ytest_tr.reshape(-1,1))
+
+        # Set class variables    
+        self.ytrain = ytrain
+        self.yval = yval
+        self.ytest = ytest
+
+        self.ytrain_tr = ytrain_tr
+        self.yval_tr = yval_tr
+        self.ytest_tr = ytest_tr
+
+        self.ytrain_sc = ytrain_sc
+        self.yval_sc = yval_sc
+        self.ytest_sc = ytest_sc
+
+        self.xtrain = xtrain
+        self.xtest = xtest
+        self.xval = xval
+
+        self.l = lmbda
+        self.xscaler = xscaler
+        self.yscaler = yscaler
+
+    def sets(self):
+        return self.xtrain, self.xval, self.xtest, self.ytrain_sc, self.yval_sc, self.ytest_sc
+
+
+
+class areal_model_new():
+    """ Class for generating data for areal models"""
+
+    def __init__(self, location, seed=42, var='uib'):
+        """
+        Inputs
+            location: specify area to train model
+            number, optional: specify desired ensemble run, integer
+            EDA_average, optional: specify if you want average of low resolution
+                ensemble runs, boolean
+            length, optional: specify number of points to sample for training, integer
+            seed, optional: specify seed, integer
+
+        Outputs
+            x_train: training feature vector, numpy array
+            y_train: training output vector, numpy array
+            x_test: testing feature vector, numpy array
+            y_test: testing output vector, numpy array
+        """
+
+        # Download data 
+        df_train_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_train_7000_'+ str(seed)+'.csv')
+        df_val_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_val_1000_'+ str(seed)+'.csv')
+        df_test_all = pd.read_csv(dir_path + 'precip-prediction/data/uib_test_2000_'+ str(seed)+'.csv')
+
+        train_ds = df_train_all.set_index(['lat', 'lon', 'time']).to_xarray()
+        val_ds = df_val_all.set_index(['lat', 'lon', 'time']).to_xarray()
+        test_ds = df_test_all.set_index(['lat', 'lon', 'time']).to_xarray()
+
+        # Apply mask
+        mask_filepath = location_sel.find_mask(location)
+        train_masked_ds = location_sel.apply_mask(train_ds, mask_filepath)
+        val_masked_ds = location_sel.apply_mask(val_ds, mask_filepath)
+        test_masked_ds = location_sel.apply_mask(test_ds, mask_filepath)
+
+
+        # Choose variables
+        if var == "all":
+            var_list = ["time", "lon", "lat", "tcwv", "slor", "d2m", "z", "EOF200U", "t2m", "EOF850U",  "EOF500U", "EOF500B2", "EOF200B",
+                        "anor", "NAO", "EOF500U2", "N34", "EOF850U2", "EOF500B2", "EOF500C" ,"EOF500C2", "tp"]
+        elif var == "uib":
+            var_list = ["time", "lon", "lat", "slor", "d2m", "z", "EOF200U", "t2m", "EOF850U",  "EOF500U", "tp"]
+        elif var == "khyber":
+            var_list = ["time", "lon", "lat", "slor", "d2m", "z", "EOF200U", "t2m", "NAO", "tp"]
+        elif var == "ngari": 
+            var_list = ["time", "lon", "lat", "slor", "d2m", "EOF200U", "t2m", "EOF850U", "NAO", "tp"]
+        elif var == "gilgit":
+            var_list = ["time", "lon", "lat", "slor", "EOF200U", "t2m", "EOF850U", "EOF500B2", "anor", "NAO", "tp"]
+
+        df_train = train_masked_ds.to_dataframe().reset_index().dropna() 
+        df_val = val_masked_ds.to_dataframe().reset_index().dropna()
+        df_test = test_masked_ds.to_dataframe().reset_index().dropna()
+
+        df_train = df_train[var_list]
+        df_val = df_val[var_list]
+        df_test = df_test[var_list]
+
+        ### Training and evaluation dataset
+
+        #### Training data
+
+        df_train["time"] = pd.to_datetime(df_train["time"])
+        df_train["time"] = pd.to_numeric(df_train["time"])
+        df_val["time"] = pd.to_datetime(df_val["time"])
+        df_val["time"] = pd.to_numeric(df_val["time"])
+        df_test["time"] = pd.to_datetime(df_test["time"])
+        df_test["time"] = pd.to_numeric(df_test["time"])
+
+    
+        # Get rid of zeros
+        df_train['tp'].loc[df_train['tp'] <= 0.0] = 0.0001
+        df_val['tp'].loc[df_val['tp'] <= 0.0] = 0.0001
+        df_test['tp'].loc[df_test['tp'] <= 0.0] = 0.0001
+
+        # X and Y
+        ytrain = df_train['tp'].values
+        xtrain = df_train.drop(columns=["tp"]).values
+        yval = df_val['tp'].values
+        xval = df_val.drop(columns=["tp"]).values
+        ytest = df_test['tp'].values
+        xtest = df_test.drop(columns=["tp"]).values
+
+        # Transformation
+        ytrain_tr, l = sp.stats.boxcox(ytrain.astype(np.float64))
+        yval_tr = sp.stats.boxcox(yval, lmbda=l)
+        ytest_tr = sp.stats.boxcox(ytest, lmbda=l)
+
+        # Features scaling
+        xscaler = MinMaxScaler()
+        xtrain = xscaler.fit_transform(xtrain.astype(np.float64))
+        xval = xscaler.transform(xval)
+        xtest = xscaler.transform(xtest)
+
+        yscaler = StandardScaler()
+        ytrain_sc = yscaler.fit_transform(ytrain_tr.reshape(-1,1))
+        yval_sc = yscaler.transform(yval_tr.reshape(-1,1))
+        ytest_sc = yscaler.transform(ytest_tr.reshape(-1,1))
+
+        # Set class variables    
+        self.ytrain = ytrain
+        self.yval = yval
+        self.ytest = ytest
+
+        self.ytrain_tr = ytrain_tr
+        self.yval_tr = yval_tr
+        self.ytest_tr = ytest_tr
+
+        self.ytrain_sc = ytrain_sc
+        self.yval_sc = yval_sc
+        self.ytest_sc = ytest_sc
+
+        self.xtrain = xtrain
+        self.xtest = xtest
+        self.xval = xval
+
+        self.l = l
+        self.xscaler = xscaler
+        self.yscaler = yscaler
+
+    def sets(self):
+        return self.xtrain, self.xval, self.xtest, self.ytrain_sc, self.yval_sc, self.ytest_sc
+    
+'''
 
 class point_model():
 
@@ -47,7 +285,7 @@ class point_model():
             var_list = ["time", "tcwv", "d2m", "EOF200U",  "t2m", "EOF850U",  "EOF500U", "EOF500B2", "EOF200B",
                 "NAO", "EOF500U2", "N34", "EOF850U2", "EOF500B", "tp",]
         else:
-            var_list =["time", "tcwv", "d2m", "t2m", "tp",]
+            var_list =["time", "tcwv", "EOF200U", "EOF500U", "tp"] #"d2m", "t2m", "tp",]
         
         # Download data and format data for string location
         if isinstance(location, str) == True:
@@ -76,15 +314,15 @@ class point_model():
         # Standardize time and select variables
         df["time"] = pd.to_datetime(df["time"])
         df["time"] = pd.to_numeric(df["time"])
-        df = df[var_list]  
-
+        df = df[var_list]
+        
         # Keep first of 70% for training
         x = df.drop(columns=["tp"]).values
         df.loc[df['tp'] <= 0.0] = 0.0001
         y = df['tp'].values
 
         xtrain, x_eval, ytrain, y_eval = train_test_split(
-            x, y, test_size=0.3, shuffle=False)
+            x, y, test_size=0.3, shuffle=False) 
 
         # Last 30% for evaluation
         xval, xtest, yval, ytest = train_test_split(x_eval, 
@@ -279,12 +517,10 @@ class areal_model_new():
 
     def sets(self):
         return self.xtrain, self.xval, self.xtest, self.ytrain_sc, self.yval_sc, self.ytest_sc
-    
-"""
 
 def areal_model(location:str, number:int=None, EDA_average=False, length=3000, seed=42,
                 maxyear=None):
-    '''
+    """
     Outputs test, validation and training data for total precipitation as a
     function of time, 2m dewpoint temperature, angle of sub-gridscale
     orography, orography, slope of sub-gridscale orography, total column water
@@ -304,7 +540,7 @@ def areal_model(location:str, number:int=None, EDA_average=False, length=3000, s
         y_train: training output vector, numpy array
         x_test: testing feature vector, numpy array
         y_test: testing output vector, numpy array
-    '''
+    """
 
     if number is not None:
         da_ensemble = era5.download_data(location, xarray=True, ensemble=True)
@@ -358,7 +594,7 @@ def areal_model(location:str, number:int=None, EDA_average=False, length=3000, s
 
 def areal_model_eval(location, lmbda, number=None, EDA_average=False, length=3000,
                      seed=42, minyear="1970", maxyear="2020"):
-    '''
+    """
     Returns data to evaluate an areal model at a given location, area and time
     period.
 
@@ -380,7 +616,7 @@ def areal_model_eval(location, lmbda, number=None, EDA_average=False, length=300
     Outputs
         x_tr: evaluation feature vector, numpy array
         y_tr: evaluation output vector, numpy array
-    '''
+    """
     if number is not None:
         da_ensemble = era5.download_data('uib', xarray=True, ensemble=True)
         da = da_ensemble.sel(number=number).drop("number")
@@ -420,7 +656,7 @@ def areal_model_eval(location, lmbda, number=None, EDA_average=False, length=300
 
 
 def average_over_coords(ds):
-    '''Take average over latitude and longitude '''
+    """Take average over latitude and longitude"""
     ds = ds.mean("lon")
     ds = ds.mean("lat")
     return ds
@@ -501,4 +737,4 @@ def shared_evaluation_sets(location:str, length=3000, seed=42):
 
     return  xval, xtest, yval, ytest
 
-"""
+'''
